f533aef197f7daff236143c7db54f1a93526f70f
Lindsay Holmwood <lindsay@holmwood.id.au>
2009-03-06 04:39:06 +1100

    only sleep for 1 second

--- stats.rb
+++ stats.rb
@@ -12,7 +12,7 @@ begin
       stats = beanstalk.stats_tube(tube_name)
       puts "#{time.to_i} [#{tube_name}] total: #{stats['total-jobs']}, waiting: #{stats['current-waiting']}, ready: #{stats['current-jobs-ready']}"
     end
-    sleep 2
+    sleep 1
   end
 rescue Beanstalk::NotConnected
   puts "beanstalk isn't up!"
