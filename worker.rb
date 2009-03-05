4f2905093425655ada5951622e6b93274882650b
Lindsay Holmwood <lindsay@holmwood.id.au>
2009-03-06 04:39:25 +1100

    execute command, and report back actual results

--- worker.rb
+++ worker.rb
@@ -7,18 +7,22 @@ begin
   @jobs    = Beanstalk::Pool.new(['localhost:11300'], 'jobs')
   @results = Beanstalk::Pool.new(['localhost:11300'], 'results')
   loop do
+    # get_job
+    # perform_job
+    # report_job
     puts 'waiting for jobs...'
     job = @jobs.reserve
     j = YAML::load(job.body)
     puts "processing #{j.inspect}"
     job.delete
-   
+
+    result = `#{j[:command]}`
+    retval = $?
+
     puts "sending results for #{j[:id]}"
     @results.yput({:id => j[:id], 
-                   :result => "Critical: 0, Warning: 0, 4 okay | value=4", 
-                   :retval => 0})
-    
-    #sleep 5
+                   :result => result, 
+                   :retval => retval.to_i})
   end
 rescue Beanstalk::NotConnected
   puts "beanstalk isn't up!"
