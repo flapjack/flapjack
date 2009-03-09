bee5c0b39f5e2eeb1d791a4fb7bbc80c42123f7c
Lindsay Holmwood <lindsay@holmwood.id.au>
2009-03-09 14:05:50 +1100

    sleep before executing, only set up 1000 checks

--- populator.rb
+++ populator.rb
@@ -5,8 +5,8 @@ require 'beanstalk-client'
 
 begin 
   @jobs = Beanstalk::Pool.new(['localhost:11300'], 'jobs')
-  2000.times do 
-    @jobs.yput({:command => "echo '#{Time.now}'", 
+  1000.times do 
+    @jobs.yput({:command => "sleep #{(1..5).to_a[rand(5)]}; echo '#{Time.now}'", 
                 :params => {}, 
                 :id => (1..100).to_a[rand(100)],
                 :frequency => [10, 30, 60, 120][rand(4)],
