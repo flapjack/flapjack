2656caab77ff21eb0e0085659a44799942805250
Lindsay Holmwood <lindsay@holmwood.id.au>
2009-03-09 18:11:48 +1100

    disown spawned processes, correctly generate worker command line, send output to /dev/null

--- wm.rb
+++ wm.rb
@@ -6,10 +6,13 @@ unless ARGV.size == 2 || ARGV.grep(/[\-\-help|\-h]/).size > 0
 end
 
 number = ARGV[0].to_i
+host = ARGV[1]
 
 puts "Spinning up #{number} workers..."
 
 number.times do
-  cmd = "ruby #{File.join(File.dirname(__FILE__), 'worker.rb')} &"
+  cmd = "ruby #{File.join(File.dirname(__FILE__), 'worker.rb')} #{host} 2>&1 >/dev/null &"
   system(cmd)
 end
+
+system("disown -a")
