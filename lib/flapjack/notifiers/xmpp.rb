448ea8a48513d899fb7992c276c8c0889e8a5d91
Lindsay Holmwood <lindsay@holmwood.id.au>
2009-03-12 03:04:50 +1100

    added xmpp notifier

---
+++ lib/flapjack/notifiers/xmpp.rb
@@ -0,0 +1,32 @@
+#!/usr/bin/env ruby
+
+require 'xmpp4r-simple'
+
+class Xmpp
+
+  def initialize(opts={})
+
+    @jid = opts[:jid]
+    @password = opts[:password]
+    unless @jid && @password 
+      raise ArgumentError, "You have to provide a username and password"
+    end
+    @xmpp = Jabber::Simple.new(@jid, @password)
+
+  end
+
+  def notify!(opts={})
+
+    raise unless opts[:who] && opts[:result]
+
+    message = <<-DESC
+      Check #{opts[:result].id} returned the status "#{opts[:result].status}".
+        http://localhost:4000/issue/#{opts[:result].object_id * -1}
+    DESC
+
+    @xmpp.deliver(opts[:who].jid, message)
+
+  end
+
+end
+
