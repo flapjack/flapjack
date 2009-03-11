c7417da02a6dcbe8f3e1ea5ad3fae5c157cae910
Lindsay Holmwood <lindsay@holmwood.id.au>
2009-03-12 04:00:17 +1100

    don't just do an echo, also exit weirdly

--- populator.rb
+++ populator.rb
@@ -8,7 +8,8 @@ number = ARGV[0] ? ARGV[0].to_i : 100
 begin 
   @jobs = Beanstalk::Pool.new(['localhost:11300'], 'jobs')
   number.times do 
-    @jobs.yput({:command => "sleep #{(1..5).to_a[rand(5)]}; echo '#{Time.now}'", 
+    #@jobs.yput({:command => "sleep #{(1..5).to_a[rand(5)]}; echo '#{Time.now}'", 
+    @jobs.yput({:command => "sleep #{(1..5).to_a[rand(5)]}; exit #{(0..2).to_a[rand(3)]}", 
                 :params => {}, 
                 :id => (1..100).to_a[rand(100)],
                 :frequency => [10, 30, 60, 120][rand(4)],
