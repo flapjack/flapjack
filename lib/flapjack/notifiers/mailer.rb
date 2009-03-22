ecf1fb30819f03f45052d1cec84f405030513ffc
Lindsay Holmwood <auxesis@theodor.(none)>
2009-03-22 14:53:23 +1100

    put notifiers in their own module

--- lib/flapjack/notifiers/mailer.rb
+++ lib/flapjack/notifiers/mailer.rb
@@ -4,29 +4,33 @@ require 'bin/common'
 require 'net/smtp'
 require 'mailfactory'
 
-class Mailer
+module Flapjack
+  module Notifiers
 
-  def notify!(opts={})
-    raise unless (opts[:who] && opts[:result])
-
-    mail = MailFactory.new
-    mail.to = opts[:who].email
-    mail.from = "notifications@flapjack-project.com"
-    mail.subject = "Check: #{opts[:result].id}, Status: #{opts[:result].status}"
-    mail.text = <<-DESC
-      Check #{opts[:result].id} returned the status "#{opts[:result].status}".
-      
-      Here was the output: 
-        #{opts[:result].output}
-
-      You can respond to this issue at:
-        http://localhost:4000/issue/#{opts[:result].object_id * -1}
-    DESC
-
-    Net::SMTP.start('localhost') do |smtp|
-      smtp.sendmail(mail.to_s, mail.from, mail.to)
+    class Mailer
+    
+      def notify!(opts={})
+        raise unless (opts[:who] && opts[:result])
+    
+        mail = MailFactory.new
+        mail.to = opts[:who].email
+        mail.from = "notifications@flapjack-project.com"
+        mail.subject = "Check: #{opts[:result].id}, Status: #{opts[:result].status}"
+        mail.text = <<-DESC
+          Check #{opts[:result].id} returned the status "#{opts[:result].status}".
+          
+          Here was the output: 
+            #{opts[:result].output}
+    
+          You can respond to this issue at:
+            http://localhost:4000/issue/#{opts[:result].object_id * -1}
+        DESC
+    
+        Net::SMTP.start('localhost') do |smtp|
+          smtp.sendmail(mail.to_s, mail.from, mail.to)
+        end
+      end
+    
     end
   end
-
 end
-
--- lib/flapjack/notifiers/xmpp.rb
+++ lib/flapjack/notifiers/xmpp.rb
@@ -2,31 +2,35 @@
 
 require 'xmpp4r-simple'
 
-class Xmpp
-
-  def initialize(opts={})
-
-    @jid = opts[:jid]
-    @password = opts[:password]
-    unless @jid && @password 
-      raise ArgumentError, "You have to provide a username and password"
+module Flapjack
+  module Notifiers
+    class Xmpp
+    
+      def initialize(opts={})
+    
+        @jid = opts[:jid]
+        @password = opts[:password]
+        unless @jid && @password 
+          raise ArgumentError, "You have to provide a username and password"
+        end
+        @xmpp = Jabber::Simple.new(@jid, @password)
+    
+      end
+    
+      def notify!(opts={})
+    
+        raise unless opts[:who] && opts[:result]
+    
+        message = <<-DESC
+          Check #{opts[:result].id} returned the status "#{opts[:result].status}".
+            http://localhost:4000/issue/#{opts[:result].object_id * -1}
+        DESC
+    
+        @xmpp.deliver(opts[:who].jid, message)
+    
+      end
+    
     end
-    @xmpp = Jabber::Simple.new(@jid, @password)
-
   end
-
-  def notify!(opts={})
-
-    raise unless opts[:who] && opts[:result]
-
-    message = <<-DESC
-      Check #{opts[:result].id} returned the status "#{opts[:result].status}".
-        http://localhost:4000/issue/#{opts[:result].object_id * -1}
-    DESC
-
-    @xmpp.deliver(opts[:who].jid, message)
-
-  end
-
 end
 
