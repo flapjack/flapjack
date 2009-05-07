a1e8010a1c2abeb2f85d0ddc026200c3802b72e0
Lindsay Holmwood <lindsay@holmwood.id.au>
2009-05-07 23:47:35 +0200

    added specs for recipients file

--- spec/notifier_options_spec.rb
+++ spec/notifier_options_spec.rb
@@ -5,6 +5,7 @@ describe NotifierOptions do
   before(:each) do 
   end
  
+  # beanstalk
   it "should accept the location of a beanstalk queue in short form" do 
     args = %w(-b localhost)
     options = NotifierOptions.parse(args)
@@ -17,6 +18,27 @@ describe NotifierOptions do
     options.host.should == "localhost"
   end
 
+  # recipients
+  it "should accept the location of a recipients file in short form" do 
+    args = %w(-b localhost -r recipients.yaml)
+    options = NotifierOptions.parse(args)
+    options.recipients.should == "recipients.yaml"
+  end
+  
+  it "should accept the location of a recipients file in long form" do 
+    args = %w(-b localhost --recipient recipients.yaml)
+    options = NotifierOptions.parse(args)
+    options.recipients.should == "recipients.yaml"
+  end
+
+  it "should exit if the recipients file doesn't exist" do 
+    args = %w(-b localhost --recipient recipients.yaml)
+    lambda { 
+      NotifierOptions.parse(args) 
+    }.should raise_error(SystemExit)
+  end
+
+  # general
   it "should exit on when asked for help in short form" do 
     args = %w(-h)
     lambda { 
