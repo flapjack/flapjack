e99813a943f52c23538d69cecc1f826c4bcec462
Lindsay Holmwood <lindsay@holmwood.id.au>
2009-05-25 01:21:59 +0200

    added simple documentation for the Result class

--- lib/flapjack/result.rb
+++ lib/flapjack/result.rb
@@ -2,20 +2,31 @@
 
 require 'ostruct'
 
+##
+# Representation of a check result, as it's popped off the beanstalk.
+# 
+# Provides several convience methods for querying the status of a result. 
+#
+# Convenience methods are used by the Notifier to determine whether a 
+# notification needs to be sent out.
 class Result < OpenStruct
-  
+
+  # Whether a check returns an ok status. 
   def ok?
     self.retval == 0
   end
 
+  # Whether a check has a warning status.
   def warning?
     self.retval == 1
   end
 
+  # Whether a check has a critical status.
   def critical?
     self.retval == 2
   end
 
+  # Human readable representation of the check's return value.
   def status
     case self.retval
     when 0 ; "ok"
@@ -24,8 +35,9 @@ class Result < OpenStruct
     end
   end
 
-  # openstruct won't respond, so we have to manually define it
+  # The id of a check result.
   def id
+    # openstruct won't respond, so we have to manually define it
     @table[:id]
   end
 
