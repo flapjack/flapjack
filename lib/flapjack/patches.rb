5468d222f0bf59f47eafc155b90da10390a22694
Lindsay Holmwood <lindsay@holmwood.id.au>
2009-05-27 18:12:12 +0200

    created single file for monkeypatches

---
+++ lib/flapjack/patches.rb
@@ -0,0 +1,17 @@
+require 'ostruct'
+require 'daemons'
+
+class OpenStruct
+  def to_h
+    @table
+  end
+end
+
+module Daemons
+  class PidFile 
+    # we override this method so creating pid files is fork-safe
+    def filename 
+      File.join(@dir, "#{@progname}#{Process.pid}.pid")
+    end
+  end
+end
