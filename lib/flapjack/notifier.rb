ad907e6ed3491fed23bb0d06351050aae9bbeb1b
Lindsay Holmwood <lindsay@holmwood.id.au>
2009-06-30 16:37:00 +0200

    pop the notifier in the correct namespace, indent appropriately

--- lib/flapjack/notifier.rb
+++ lib/flapjack/notifier.rb
@@ -2,33 +2,34 @@
 
 require 'ostruct'
 
-class Notifier
- 
-  attr_reader :recipients, :log
-
-  def initialize(opts={})
-    @log = opts[:logger]
-    raise "you have to specify a logger" unless @log
-    @recipients = (opts[:recipients] || [])
-    
-    @notifiers = []
-    if opts[:notifiers]
-	    opts[:notifiers].each do |n|
-	      @notifiers << n
-	      @log.info("using the #{n.class.to_s.split("::").last} notifier")
-	    end
-    else
-      @log.warning("there are no notifiers")
-    end
-  end
+module Flapjack
+  class Notifier
+   
+    attr_reader :recipients, :log
   
-  def notify!(result)
-    @notifiers.each do |n|
-      @recipients.each do |recipient|
-        @log.info("Notifying #{recipient.name} via #{n.class} about check #{result.id}")
-        n.notify(:result => result, :who => recipient)
+    def initialize(opts={})
+      @log = opts[:logger]
+      raise "you have to specify a logger" unless @log
+      @recipients = (opts[:recipients] || [])
+      
+      @notifiers = []
+      if opts[:notifiers]
+        opts[:notifiers].each do |n|
+          @notifiers << n
+          @log.info("using the #{n.class.to_s.split("::").last} notifier")
+        end
+      else
+        @log.warning("there are no notifiers")
+      end
+    end
+    
+    def notify!(result)
+      @notifiers.each do |n|
+        @recipients.each do |recipient|
+          @log.info("Notifying #{recipient.name} via #{n.class} about check #{result.id}")
+          n.notify(:result => result, :who => recipient)
+        end
       end
     end
   end
 end
-
