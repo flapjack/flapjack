dd2b1547607fb7b27e1e6ad5a0cfea235b1939bf
Lindsay Holmwood <lindsay@holmwood.id.au>
2009-11-22 14:44:18 +0100

    added commented out property

--- lib/flapjack/models/event.rb
+++ lib/flapjack/models/event.rb
@@ -7,6 +7,7 @@ class Event
 
   property :id, Serial, :key => true
   property :check_id, Integer, :nullable => false
+  #property :check_output, Text, :nullable => false
 
   # dm-timestamps
   property :created_at, DateTime
