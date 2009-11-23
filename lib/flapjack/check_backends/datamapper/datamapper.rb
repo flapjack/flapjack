d501d1a45d3b424a8fa2f2083a2dcacb9b912ead
Lindsay Holmwood <lindsay@holmwood.id.au>
2009-11-23 22:42:03 +0100

    filled in datamapper backend

--- lib/flapjack/check_backends/datamapper/datamapper.rb
+++ lib/flapjack/check_backends/datamapper/datamapper.rb
@@ -15,6 +15,41 @@ module Flapjack
       def initialize(options={})
         @options = options
         @config = OpenStruct.new(options)
+        @log = @config.log
+        connect
+      end
+
+      def any_parents_failed?(result)
+        check = Check.get(result.id)
+        check.worst_parent_status != 0
+      end
+
+      def save(result)
+        check = Check.get(result.id)
+        check.status = result.status
+        check.save
+      end
+
+      def create_event(result)
+        event = Event.new(:check_id => result.id)
+        event.save
+      end
+
+      private 
+      def connect
+        raise ArgumentError, "Database URI wasn't specified" unless @config.uri
+        DataMapper.setup(:default, @config.uri)
+        validate
+      end
+
+      def validate
+        begin
+          DataMapper.repository(:default).adapter.execute("SELECT 'id' FROM 'checks';")
+        rescue Sqlite3Error => e
+          @log.warning("The specified database doesn't appear to have any structure!")
+          @log.warning("Exiting.")
+          raise
+        end
       end
     end
   end
