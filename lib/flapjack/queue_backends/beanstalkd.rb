77fd9414ef1f0f7d9a6940c4889c1f103349b1b1
Lindsay Holmwood <lindsay@holmwood.id.au>
2009-11-23 16:13:39 +0100

    added a class for wrapping results off a queue. return a nicely wrapper result when getting next result off queue. handle deleting the job

--- lib/flapjack/queue_backends/beanstalkd.rb
+++ lib/flapjack/queue_backends/beanstalkd.rb
@@ -18,7 +18,58 @@ module Flapjack
 
       def next
         job = @queue.reserve # blocks
+        Flapjack::QueueBackends::Result.new(:job => job)
       end
+
+      def delete(job)
+        job.delete
+      end
+    end
+  end
+end
+
+module Flapjack
+  module QueueBackends
+    class Result
+      def initialize(options={})
+        @job = OpenStruct.new(YAML::load(options[:job].body))
+      end
+  
+      # Whether a check returns an ok status. 
+      def ok?
+        @job.retval == 0
+      end
+    
+      # Whether a check has a warning status.
+      def warning?
+        @job.retval == 1
+      end
+    
+      # Whether a check has a critical status.
+      def critical?
+        @job.retval == 2
+      end
+    
+      # Human readable representation of the check's return value.
+      def status
+        case @job.retval
+        when 0 ; "ok"
+        when 1 ; "warning"
+        when 2 ; "critical"
+        end
+      end
+    
+      def id
+        # openstruct won't respond, so we have to manually define it
+        @job.id
+      end
+
+      def save 
+      end
+
+      def delete
+      end
+  
     end
   end
 end
