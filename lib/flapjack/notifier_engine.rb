1f7bc9be1198292cbed456b8d3416202c8dfe2de
Lindsay Holmwood <lindsay@holmwood.id.au>
2009-11-23 22:43:06 +0100

    renamed Flapjack::Notifier to Flapjack::NotifierEngine

---
+++ lib/flapjack/notifier_engine.rb
@@ -0,0 +1,40 @@
+#!/usr/bin/env ruby
+
+require 'ostruct'
+
+module Flapjack
+  class NotifierEngine
+   
+    attr_reader :recipients, :log, :notifiers
+  
+    def initialize(opts={})
+      @log = opts[:logger]
+      raise "you have to specify a logger" unless @log
+      @recipients = (opts[:recipients] || [])
+      
+      @notifiers = []
+      if opts[:notifiers]
+        opts[:notifiers].each do |n|
+          @notifiers << n
+          @log.info("using the #{n.class.to_s.split("::").last} notifier")
+        end
+      else
+        @log.warning("There are no notifiers! flapjack-notifier won't be useful.")
+      end
+    end
+   
+    def notify!(options={})
+      result = options[:result]
+      event  = options[:event]
+
+      raise ArgumentError, "A result + event were not passed!" unless result && event
+
+      @notifiers.each do |n|
+        @recipients.each do |recipient|
+          @log.info("Notifying #{recipient.name} via #{n.class} about check #{result.id}")
+          n.notify(:result => result, :who => recipient, :event => event)
+        end
+      end
+    end
+  end
+end
