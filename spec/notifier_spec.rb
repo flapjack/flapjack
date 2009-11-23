ac03c2bf3a0e5752e9fcbe2eb8ec713f286e2106
Lindsay Holmwood <lindsay@holmwood.id.au>
2009-11-23 23:36:23 +0100

    refactored test for NotifierEngine

--- spec/notifier_spec.rb
+++ spec/notifier_spec.rb
@@ -1,5 +1,5 @@
-require File.join(File.dirname(__FILE__), '..', 'lib', 'flapjack', 'notifier')
-require File.join(File.dirname(__FILE__), '..', 'lib', 'flapjack', 'result')
+require File.join(File.dirname(__FILE__), '..', 'lib', 'flapjack', 'notifier_engine')
+require File.join(File.dirname(__FILE__), '..', 'lib', 'flapjack', 'queue_backends', 'result')
 require File.join(File.dirname(__FILE__), 'helpers')
 
 describe "running the notifier" do 
@@ -9,50 +9,53 @@ describe "running the notifier" do
  
   it "should complain if no logger has been specified" do 
     lambda {
-      n = Flapjack::Notifier.new
+      n = Flapjack::NotifierEngine.new
     }.should raise_error(RuntimeError)
   end
   
   it "should have a blank recipients list if none specified" do 
-    n = Flapjack::Notifier.new(:logger => MockLogger.new)
+    n = Flapjack::NotifierEngine.new(:logger => MockLogger.new)
     n.recipients.should_not be_nil
     n.recipients.size.should == 0
   end
 
   it "should warn if no notifiers have been specified" do 
-    n = Flapjack::Notifier.new(:logger => MockLogger.new)
+    n = Flapjack::NotifierEngine.new(:logger => MockLogger.new)
     n.log.messages.last.should =~ /no notifiers/
   end
 
   it "should log when adding a new notifier" do
-    mock_notifier = mock("MockFlapjack::Notifier")
-    n = Flapjack::Notifier.new(:logger => MockLogger.new, 
+    mock_notifier = mock("MockFlapjack::NotifierEngine")
+    n = Flapjack::NotifierEngine.new(:logger => MockLogger.new, 
                      :notifiers => [mock_notifier])
     n.log.messages.last.should =~ /using the (.+) notifier/
   end
 
   it "should call notify on each notifier when notifying" do 
-    mock_notifier = mock("MockFlapjack::Notifier")
+    mock_notifier = mock("MockFlapjack::NotifierEngine")
     mock_notifier.should_receive(:notify)
 
-    n = Flapjack::Notifier.new(:logger => MockLogger.new, 
+    result = Flapjack::QueueBackends::Result.new(:result => {:check_id => 12345})
+
+    n = Flapjack::NotifierEngine.new(:logger => MockLogger.new, 
                      :notifiers => [mock_notifier],
                      :recipients => [OpenStruct.new({:name => "John Doe"})])
-    n.notify!(Flapjack::Result.new(:id => 'mock result'), nil)
+    n.notify!(:result => result, :event => true)
   end
 
   it "should log notification on each notifier" do 
-    mock_notifier = mock("MockFlapjack::Notifier")
+    mock_notifier = mock("MockFlapjack::NotifierEngine")
     mock_notifier.stub!(:notify)
 
-    n = Flapjack::Notifier.new(:logger => MockLogger.new, 
-                     :notifiers => [mock_notifier],
-                     :recipients => [OpenStruct.new({:name => "John Doe"})])
-    n.notify!(Flapjack::Result.new(:id => '12345'), nil)
+    result = Flapjack::QueueBackends::Result.new(:result => {:check_id => 12345})
+
+    n = Flapjack::NotifierEngine.new(:logger => MockLogger.new, 
+                                     :notifiers => [mock_notifier],
+                                     :recipients => [OpenStruct.new({:name => "John Doe"})])
+    n.notify!(:result => result, :event => true)
     n.log.messages.last.should =~ /12345/
     n.log.messages.last.should =~ /John Doe/
   end
 end
 
 
-def puts(*args) ; end
