639753c2c4662ac86b4e2787e7b81e2c08f374da
Lindsay Holmwood <lindsay@holmwood.id.au>
2009-11-23 12:46:28 +0100

    added mock mailer

---
+++ spec/notifier-directories/spoons/mailer/init.rb
@@ -0,0 +1,18 @@
+#!/usr/bin/env ruby 
+
+module Flapjack
+  module Notifiers
+    class Mailer
+
+      def initialize(opts={})
+      end
+
+      def notify(opts={})
+      end
+    
+    end
+  end
+end
+
+
+
