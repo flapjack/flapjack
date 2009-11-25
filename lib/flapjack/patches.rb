05bdc4625216e5bf92ebca217df2a68749fc567e
Lindsay Holmwood <lindsay@holmwood.id.au>
2009-11-25 02:13:28 +0100

    added camel_case method to strings, extracted from extlib

--- lib/flapjack/patches.rb
+++ lib/flapjack/patches.rb
@@ -28,3 +28,12 @@ module Log4r
     end
   end
 end
+
+# extracted from Extlib. 
+# FIXME: what's the licensing here?
+class String
+  def camel_case                                                                                                                                    
+    return self if self !~ /_/ && self =~ /[A-Z]+.*/                                                                                                
+    split('_').map{|e| e.capitalize}.join                                                                                                           
+  end   
+end
