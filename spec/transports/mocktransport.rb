f89168df4a5fa81a5ae4c38b66a8f0ec7355edaf
Lindsay Holmwood <lindsay@holmwood.id.au>
2009-11-25 01:27:34 +0100

    specify class explicityl

--- spec/transports/mocktransport.rb
+++ spec/transports/mocktransport.rb
@@ -14,14 +14,14 @@ module Flapjack
       %w(delete put).each do |method|
         class_eval <<-HERE
           def #{method}(*args)
-            @log.info("method #{method} was called on #{self.class.to_s.split('::').last}")
+            @log.info("method #{method} was called on Mocktransport")
           end
         HERE
       end
 
       # exactly the same as above, just returns a real object
       def next
-        @log.info("method next was called on #{self.class.to_s.split('::').last}")
+        @log.info("method next was called on Mocktransport")
         MockResult.new(@options)
       end
 
@@ -43,14 +43,14 @@ module Flapjack
       %w(warning? critical? any_parents_failed? save check_id frequency).each do |method|
         class_eval <<-HERE
           def #{method}(*args)
-            @log.info("method #{method} was called on #{self.class.to_s.split('::').last}")
+            @log.info("method #{method} was called on MockResult")
           end
         HERE
       end
 
       # exactly the same as above, just returns a real object
       def command(*args)
-        @log.info("method command was called on #{self.class.to_s.split('::').last}")
+        @log.info("method command was called on MockResult")
         "echo foo"
       end
     end
