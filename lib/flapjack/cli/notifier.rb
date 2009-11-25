13c06f3624154d3e9352e53b349714554b3d4d86
Lindsay Holmwood <lindsay@holmwood.id.au>
2009-11-25 16:24:39 +0100

    renamespaced Flapjack::NotifierOptions to Flapjack::Notifier::Options

--- lib/flapjack/cli/notifier.rb
+++ lib/flapjack/cli/notifier.rb
@@ -6,81 +6,83 @@ require 'log4r'
 require 'log4r/outputter/syslogoutputter'
 
 module Flapjack
-  class NotifierOptions
-    def self.parse(args)
-      options = OpenStruct.new
-      opts = OptionParser.new do |opts|
-        # the available command line options
-        opts.on('-b', '--beanstalk HOST', 'location of the beanstalkd') do |host|
-          options.host = host
+  module Notifier
+    class Options
+      def self.parse(args)
+        options = OpenStruct.new
+        opts = OptionParser.new do |opts|
+          # the available command line options
+          opts.on('-b', '--beanstalk HOST', 'location of the beanstalkd') do |host|
+            options.host = host
+          end
+          opts.on('-p', '--port PORT', 'beanstalkd port') do |port|
+            options.port = port.to_i
+          end
+          opts.on('-r', '--recipients FILE', 'recipients file') do |recipients|
+            options.recipients = recipients.to_s
+          end
+          opts.on('-c', '--config FILE', 'config file') do |config|
+            options.config_filename = config.to_s
+          end
+          opts.on('-d', '--database-uri URI', 'location of the checks database') do |db|
+            options.database_uri = db.to_s
+          end
+          opts.on_tail("-h", "--help", "Show this message") do
+            puts opts
+            exit
+          end
         end
-        opts.on('-p', '--port PORT', 'beanstalkd port') do |port|
-          options.port = port.to_i
-        end
-        opts.on('-r', '--recipients FILE', 'recipients file') do |recipients|
-          options.recipients = recipients.to_s
-        end
-        opts.on('-c', '--config FILE', 'config file') do |config|
-          options.config_filename = config.to_s
-        end
-        opts.on('-d', '--database-uri URI', 'location of the checks database') do |db|
-          options.database_uri = db.to_s
-        end
-        opts.on_tail("-h", "--help", "Show this message") do
+  
+        # parse the options
+        begin
+          opts.parse!(args)
+        rescue OptionParser::MissingArgument => e
+          # if an --option is missing it's argument
+          puts e.message.capitalize + "\n\n"
           puts opts
-          exit
+          exit 1
         end
-      end
-
-      # parse the options
-      begin
-        opts.parse!(args)
-      rescue OptionParser::MissingArgument => e
-        # if an --option is missing it's argument
-        puts e.message.capitalize + "\n\n"
-        puts opts
-        exit 1
-      end
- 
-      # default the host + port
-      options.host ||= 'localhost'
-      options.port ||= 11300
+   
+        # default the host + port
+        options.host ||= 'localhost'
+        options.port ||= 11300
+    
+        @errors = []
+        # check that recipients file exists
+        if options.recipients
+          unless File.exists?(options.recipients.to_s)
+            @errors << "The specified recipients file doesn't exist!"
+          end
+        else
+          @errors << "You need to specify a recipients file with [-r|--recipients]."
+        end
   
-      @errors = []
-      # check that recipients file exists
-      if options.recipients
-        unless File.exists?(options.recipients.to_s)
-          @errors << "The specified recipients file doesn't exist!"
+        # check that config file exists
+        if options.config_filename 
+          unless File.exists?(options.config_filename.to_s)
+            @errors << "The specified config file doesn't exist!"
+          end
+        else
+          options.config_filename ||= "/etc/flapjack/flapjack-notifier.yaml"
+          unless File.exists?(options.config_filename.to_s)
+            @errors << "The default config file (#{options.config_filename}) doesn't exist!"
+            @errors << "Set one up, or specify one with [-c|--config]."
+          end
         end
-      else
-        @errors << "You need to specify a recipients file with [-r|--recipients]."
-      end
-
-      # check that config file exists
-      if options.config_filename 
-        unless File.exists?(options.config_filename.to_s)
-          @errors << "The specified config file doesn't exist!"
-        end
-      else
-        options.config_filename ||= "/etc/flapjack/flapjack-notifier.yaml"
-        unless File.exists?(options.config_filename.to_s)
-          @errors << "The default config file (#{options.config_filename}) doesn't exist!"
-          @errors << "Set one up, or specify one with [-c|--config]."
-        end
-      end
-
-      # if there are errors, print them out and exit
-      if @errors.size > 0
-        puts "Errors:"
-        @errors.each do |error|
-          puts "  - #{error}"
-        end
-        puts
-        puts opts
-        exit 2
-      end
   
-      options
+        # if there are errors, print them out and exit
+        if @errors.size > 0
+          puts "Errors:"
+          @errors.each do |error|
+            puts "  - #{error}"
+          end
+          puts
+          puts opts
+          exit 2
+        end
+    
+        options
+      end
     end
   end
  
--- spec/notifier_options_spec.rb
+++ spec/notifier_options_spec.rb
@@ -1,113 +1,110 @@
 require File.join(File.dirname(__FILE__), '..', 'lib', 'flapjack', 'cli', 'notifier')
 
-describe Flapjack::NotifierOptions do 
- 
-  before(:each) do 
-  end
+describe Flapjack::Notifier::Options do 
  
   # beanstalk
   it "should accept the location of a beanstalk queue in short form" do 
     args = %w(-b localhost -r spec/recipients.yaml -c spec/flapjack-notifier.yaml)
-    options = Flapjack::NotifierOptions.parse(args)
+    options = Flapjack::Notifier::Options.parse(args)
     options.host.should == "localhost"
   end
   
   it "should accept the location of a beanstalk queue in long form" do 
     args = %w(--beanstalk localhost -r spec/recipients.yaml -c spec/flapjack-notifier.yaml)
-    options = Flapjack::NotifierOptions.parse(args)
+    options = Flapjack::Notifier::Options.parse(args)
     options.host.should == "localhost"
   end
 
   it "should set a default beanstalk host" do 
     args = %w(-r spec/recipients.yaml -c spec/flapjack-notifier.yaml)
-    options = Flapjack::NotifierOptions.parse(args)
+    options = Flapjack::Notifier::Options.parse(args)
     options.host.should == 'localhost'
   end
 
   # beanstalk port
   it "should accept a specified beanstalk port in short form" do 
     args = %w(-b localhost -p 11340 -r spec/recipients.yaml -c spec/flapjack-notifier.yaml)
-    options = Flapjack::NotifierOptions.parse(args)
+    options = Flapjack::Notifier::Options.parse(args)
     options.port.should == 11340
   end
   
   it "should accept a specified beanstalk port in long form" do 
     args = %w(-b localhost --port 11399 -r spec/recipients.yaml -c spec/flapjack-notifier.yaml)
-    options = Flapjack::NotifierOptions.parse(args)
+    options = Flapjack::Notifier::Options.parse(args)
     options.port.should == 11399
   end
 
   it "should set a default beanstalk port" do 
     args = %w(-b localhost -r spec/recipients.yaml -c spec/flapjack-notifier.yaml)
-    options = Flapjack::NotifierOptions.parse(args)
+    options = Flapjack::Notifier::Options.parse(args)
     options.port.should == 11300
   end
 
   # recipients
   it "should accept the location of a recipients file in short form" do 
     args = %w(-b localhost -r spec/recipients.yaml -c spec/flapjack-notifier.yaml)
-    options = Flapjack::NotifierOptions.parse(args)
+    options = Flapjack::Notifier::Options.parse(args)
     options.recipients.should == "spec/recipients.yaml"
   end
   
   it "should accept the location of a recipients file in long form" do 
     args = %w(-b localhost --recipient spec/recipients.yaml -c spec/flapjack-notifier.yaml)
-    options = Flapjack::NotifierOptions.parse(args)
+    options = Flapjack::Notifier::Options.parse(args)
     options.recipients.should == "spec/recipients.yaml"
   end
 
   it "should exit if the recipients file doesn't exist" do 
     args = %w(-b localhost -r spec/wangity.yaml -c spec/flapjack-notifier.yaml)
     lambda { 
-      Flapjack::NotifierOptions.parse(args) 
+      Flapjack::Notifier::Options.parse(args) 
     }.should raise_error(SystemExit)
   end
 
   it "should exit if the recipients file isn't specified" do 
     args = %w(-b localhost -c spec/flapjack-notifier.yaml)
     lambda { 
-      Flapjack::NotifierOptions.parse(args) 
+      Flapjack::Notifier::Options.parse(args) 
     }.should raise_error(SystemExit)
   end
 
   # config 
   it "should accept the location of a config file in short form" do 
     args = %w(-b localhost -r spec/recipients.yaml -c spec/flapjack-notifier.yaml)
-    options = Flapjack::NotifierOptions.parse(args)
+    options = Flapjack::Notifier::Options.parse(args)
     options.config_filename.should == "spec/flapjack-notifier.yaml"
   end
 
   it "should accept the location of a config file in long form" do 
     args = %w(-b localhost -r spec/recipients.yaml --config spec/flapjack-notifier.yaml)
-    options = Flapjack::NotifierOptions.parse(args)
+    options = Flapjack::Notifier::Options.parse(args)
     options.config_filename.should == "spec/flapjack-notifier.yaml"
   end
 
   it "should provide a default config file if not specified" do 
     args = %w(-b localhost -r spec/recipients.yaml -c spec/flapjack-notifier.yaml)
-    options = Flapjack::NotifierOptions.parse(args)
+    options = Flapjack::Notifier::Options.parse(args)
     options.config_filename.should_not be_nil
   end
 
   it "should exit if the config file doesn't exist" do 
     args = %w(-b localhost --config spec/wangity.yaml)
     lambda { 
-      Flapjack::NotifierOptions.parse(args) 
+      Flapjack::Notifier::Options.parse(args) 
     }.should raise_error(SystemExit)
   end
 
   # general
-  it "should exit on when asked for help in short form" do 
+  it "should exit when asked for help in short form" do 
     args = %w(-h)
     lambda { 
-      Flapjack::NotifierOptions.parse(args) 
+      Flapjack::Notifier::Options.parse(args) 
     }.should raise_error(SystemExit)
   end
 
-  it "should exit on when asked for help in long form" do 
+  it "should exit when asked for help in long form" do 
     args = %w(--help)
     lambda { 
-      Flapjack::NotifierOptions.parse(args) 
+      Flapjack::Notifier::Options.parse(args) 
     }.should raise_error(SystemExit)
   end
 end
