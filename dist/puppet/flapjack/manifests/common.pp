2b763d702fff3dc7e4e5ee1f5b59782c27aeaf05
Lindsay Holmwood <lindsay@holmwood.id.au>
2011-01-14 16:29:18 +1100

    Refactored horrible execs into file resources. Symlink installed gem root to /usr/lib/flapjack.

--- dist/puppet/flapjack/manifests/common.pp
+++ dist/puppet/flapjack/manifests/common.pp
@@ -3,7 +3,7 @@ class flapjack::common {
   include ruby::rubygems
   include sqlite3::dev
 
-  $version = "0.5.2"
+  $version = "0.5.3"
 
   package { "flapjack":
     ensure   => $version,
@@ -13,7 +13,7 @@ class flapjack::common {
   }
 
   file { "/var/run/flapjack":
-    ensure  => present,
+    ensure  => directory,
     mode    => 777,
     require => [ Package["flapjack"] ],
   }
@@ -23,26 +23,39 @@ class flapjack::common {
     require => [ Package["flapjack"] ],
   }
 
+  exec { "symlink-latest-flapjack-gem":
+    command => "ln -sf $(dirname $(dirname $(dirname $(gem which flapjack/patches)))) /usr/lib/flapjack",
+    path    => "/bin:/usr/bin:/usr/local/bin:/sbin:/usr/sbin:/usr/local/sbin",
+    unless  => "readlink /usr/lib/flapjack |grep -E '$version$'",
+    require => [ Package["flapjack"] ]
+  }
+
   exec { "populate-etc-flapjack":
-    command => "cp $(dirname $(dirname $(dirname $(gem which flapjack/patches))))/etc/flapjack/* /etc/flapjack",
-    creates => "/etc/flapjack/flapjack-notifier.conf.example",
+    command => "cp $(dirname $(dirname $(dirname $(gem which flapjack))))/dist/etc/flapjack/* /etc/flapjack",
+    creates => [ "/etc/flapjack/flapjack-notifier.conf.example", "/etc/flapjack/recipients.conf.example" ],
     path    => "/bin:/usr/bin:/usr/local/bin:/sbin:/usr/sbin:/usr/local/sbin",
     require => [ File["/etc/flapjack"],
                  Package["flapjack"] ],
   }
 
-  exec { "populate-etc-defaults-flapjack":
-    command => "cp $(dirname $(dirname $(dirname $(gem which flapjack/patches))))/etc/default/* /etc/default",
-    creates => [ "/etc/default/flapjack-worker", "/etc/default/flapjack-notifier" ],
-    path    => "/bin:/usr/bin:/usr/local/bin:/sbin:/usr/sbin:/usr/local/sbin",
-    require => [ Package["flapjack"] ],
+  file { "/etc/default/flapjack-workers":
+    source  => "/usr/lib/flapjack/dist/etc/default/flapjack-workers",
+    require => [ Exec["symlink-latest-flapjack-gem"] ],
   }
 
-  exec { "populate-etc-init.d-flapjack":
-    command => "cp $(dirname $(dirname $(dirname $(gem which flapjack/patches))))/etc/init.d/* /etc/init.d",
-    creates => [ "/etc/init.d/flapjack-worker", "/etc/init.d/flapjack-notifier" ],
-    path    => "/bin:/usr/bin:/usr/local/bin:/sbin:/usr/sbin:/usr/local/sbin",
-    require => [ Package["flapjack"] ],
+  file { "/etc/default/flapjack-notifier":
+    source  => "/usr/lib/flapjack/dist/etc/default/flapjack-notifier",
+    require => [ Exec["symlink-latest-flapjack-gem"] ],
+  }
+
+  file { "/etc/init.d/flapjack-workers":
+    source  => "/usr/lib/flapjack/dist/etc/init.d/flapjack-workers",
+    require => [ Exec["symlink-latest-flapjack-gem"] ],
+  }
+
+  file { "/etc/init.d/flapjack-notifier":
+    source  => "/usr/lib/flapjack/dist/etc/init.d/flapjack-notifier",
+    require => [ Exec["symlink-latest-flapjack-gem"] ],
   }
 
 }
