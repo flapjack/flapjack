9c63f295ea2a4fb13e5b1f3ba03bbaed5094ffc1
Lindsay Holmwood <lindsay@holmwood.id.au>
2011-01-17 18:41:30 +1100

    Group process management tasks into a class, and apply it to World

--- features/support/env.rb
+++ features/support/env.rb
@@ -6,3 +6,37 @@ require 'pathname'
 require 'yajl'
 require 'beanstalk-client'
 
+class ProcessManagement
+  def kill_lingering_daemons
+    Process.kill("KILL", @beanstalk.pid)
+  end
+
+  # http://weblog.jamisbuck.org/assets/2006/9/25/gdb.rb
+  def read_until_done(pipe, verbose=false)
+    output = []
+    line    = ""
+    while data = IO.select([pipe], nil, nil, 1) do
+      next if data.empty?
+      char = pipe.read(1)
+      break if char.nil?
+
+      line << char
+      if line[-1] == ?\n
+        puts line if verbose
+        output << line
+        line = ""
+      end
+    end
+
+    output
+  end
+
+end
+
+After do |scenario|
+  kill_lingering_daemons
+end
+
+World do
+  ProcessManagement.new
+end
