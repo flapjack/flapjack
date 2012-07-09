7291b773b24e77dc28b9b9dcd6d1002de46ef145
Lindsay Holmwood <lindsay@holmwood.id.au>
2012-07-09 14:10:50 +1000

    Boil down notifier to its functional core:
     - Be opinionated in how things are set up (one logger, one persistence
       backend, no notifiers, one queue, two filters.
     - Remove various setup methods, as they're not needed.
     - Make logging descriptive of what's actually happening.
     - Change focus from processing 'results' to 'events'.
     - Create abstraction for events.

--- lib/flapjack/applications/notifier.rb
+++ lib/flapjack/applications/notifier.rb
@@ -1,222 +1,104 @@
 #!/usr/bin/env ruby
 
-$: << File.expand_path(File.join(File.dirname(__FILE__), '..', '..'))
 require 'log4r'
 require 'log4r/outputter/syslogoutputter'
+require 'redis'
 require 'flapjack/patches'
-require 'flapjack/notifier_engine'
+require 'flapjack/filters/ok'
+require 'json'
 
 module Flapjack
-  module Notifier
-    class Application
+  class Event
+    def initialize(attrs={})
+      @attrs = attrs
+    end
 
-      # boots the notifier
-      def self.run(options={})
-        app = self.new(options)
-        app.setup_config
-        app.setup_loggers
-        app.setup_notifiers
-        app.setup_notifier_engine
-        app.setup_recipients
-        app.setup_persistence
-        app.setup_queues
-        app.setup_filters
+    def warning?
+      @attrs['state'] == 'warning'
+    end
 
-        app
+    def critical?
+      @attrs['state'] == 'critical'
+    end
+
+    def host
+      @attrs['host']
+    end
+
+    def service
+      @attrs['service']
+    end
+
+    def id
+      host + ';' + service
+    end
+  end
+
+  class Events
+    def initialize
+      @redis = ::Redis.new
+      @key   = 'events'
+    end
+
+    def next
+      raw   = @redis.blpop(@key).last
+      event = ::JSON.parse(raw)
+      Flapjack::Event.new(event)
+    end
+
+    def size
+      @redis.llen(@key)
+    end
+  end
+end
+
+module Flapjack
+  class Notifier
+    # Boots the notifier.
+    def self.run(options={})
+      self.new(options)
+    end
+
+    attr_accessor :log
+
+    def initialize(options={})
+      @options     = options
+      @persistence = ::Redis.new
+      @events      = Flapjack::Events.new
+
+      @log = Log4r::Logger.new("notifier")
+      @log.add(Log4r::StdoutOutputter.new("notifier"))
+      @log.add(Log4r::SyslogOutputter.new("notifier"))
+
+
+      options = { :log => @log, :persistence => @persistence }
+      @filters = []
+      @filters << Flapjack::Filters::Ok.new(options)
+      @filters << Flapjack::Filters::Acknowledged.new(options)
+    end
+
+    def process_result
+      @log.info("Waiting for event...")
+      event = @events.next
+
+      @log.debug("#{@events.size} events waiting on the queue")
+
+      #@log.info("Storing event.")
+      #@persistence.save(event)
+
+      block = @filters.find {|filter| filter.block?(event) }
+      if not block
+        @log.info("Sending notifications for event #{event.host};#{event.service}")
+      else
+        @log.info("Not sending notifications for event #{event.host};#{event.service} because the #{block.name} filter blocked")
       end
+    end
 
-      attr_accessor :log, :recipients
-
-      def initialize(options={})
-        @log = options[:log]
-        @notifier_directories = options[:notifier_directories]
-        @filter_directories = options[:filter_directories]
-        @options = options
+    def main
+      @log.info("Booting main loop.")
+      loop do
+        process_result
       end
-
-      def setup_loggers
-        unless @log
-          @log = Log4r::Logger.new("notifier")
-          @log.add(Log4r::StdoutOutputter.new("notifier"))
-          @log.add(Log4r::SyslogOutputter.new("notifier"))
-        end
-      end
-
-      def setup_config
-        @config = OpenStruct.new(@options)
-      end
-
-      def setup_notifiers
-        @notifier_directories ||= []
-
-        default_directory = File.expand_path(File.join(File.dirname(__FILE__), '..', 'notifiers'))
-        # the default directory should be the last in the list
-        if @notifier_directories.include?(default_directory)
-          @notifier_directories << @notifier_directories.delete(default_directory)
-        else
-          @notifier_directories << default_directory
-        end
-
-        # filter to the directories that actually exist
-        @notifier_directories = @notifier_directories.find_all do |dir|
-          if File.exists?(dir)
-            true
-          else
-            @log.warning("Notifiers directory #{dir} doesn't exist. Skipping.")
-            false
-          end
-        end
-
-        @notifiers = []
-
-        # load up the notifiers and pass a config
-        @config.notifiers.each_pair do |notifier, config|
-          filenames = @notifier_directories.map {|dir| File.join(dir, notifier.to_s, 'init' + '.rb')}
-          filename = filenames.find {|filename| File.exists?(filename)}
-
-          if filename
-            @log.info("Loading the #{notifier.to_s.capitalize} notifier (from #{filename})")
-            require filename
-            config.merge!(:log => @log)
-            notifier = Flapjack::Notifiers.const_get("#{notifier.to_s.capitalize}").new(config)
-            @notifiers << notifier
-          else
-            @log.warning("Flapjack::Notifiers::#{notifier.to_s.capitalize} doesn't exist!")
-          end
-        end
-
-      end
-
-      def setup_notifier_engine
-        options = { :log => @log, :notifiers => @notifiers }
-        @notifier_engine = Flapjack::NotifierEngine.new(options)
-      end
-
-      def setup_recipients
-        @recipients ||= []
-
-        @recipients += (@config.recipients || [])
-        # so poking at a recipient within notifiers is easier
-        @recipients.map! do |recipient|
-          OpenStruct.new(recipient)
-        end
-      end
-
-      def setup_persistence
-        defaults = { :backend => :data_mapper,
-                     :log => @log }
-        config = defaults.merge(@config.persistence || {})
-        basedir = config.delete(:basedir) || File.join(File.dirname(__FILE__), '..', 'persistence')
-
-        filename = File.join(basedir, "#{config[:backend]}.rb")
-        class_name = config[:backend].to_s.camel_case
-
-        @log.info("Loading the #{class_name} persistence backend")
-
-        begin
-          require filename
-          @persistence = Flapjack::Persistence.const_get(class_name).new(config)
-        rescue LoadError => e
-          @log.warning("Attempted to load #{class_name} persistence backend, but it doesn't exist!")
-          @log.warning("Exiting.")
-          raise # preserves original exception
-        end
-
-      end
-
-      def setup_queues
-        defaults = { :backend => :beanstalkd,
-                     :host => 'localhost',
-                     :port => '11300',
-                     :queue_name => 'results',
-                     :log => @log }
-        config = defaults.merge(@config.transport || {})
-        basedir = config.delete(:basedir) || File.join(File.dirname(__FILE__), '..', 'transports')
-
-        class_name = config[:backend].to_s.camel_case
-        filename = File.join(basedir, "#{config[:backend]}.rb")
-
-        @log.info("Loading the #{class_name} transport")
-
-        begin
-          require filename
-          @results_queue = Flapjack::Transport.const_get(class_name).new(config)
-        rescue LoadError => e
-          @log.warning("Attempted to load #{class_name} transport, but it doesn't exist!")
-          @log.warning("Exiting.")
-          raise # preserves original exception
-        end
-      end
-
-      def setup_filters
-        @filter_directories ||= []
-
-        default_directory = File.expand_path(File.join(File.dirname(__FILE__), '..', 'filters'))
-        # the default directory should be the last in the list
-        if @filter_directories.include?(default_directory)
-          @filter_directories << @filter_directories.delete(default_directory)
-        else
-          @filter_directories << default_directory
-        end
-
-        # filter to the directories that actually exist
-        @filter_directories = @filter_directories.find_all do |dir|
-          if File.exists?(dir)
-            true
-          else
-            @log.warning("Filters directory #{dir} doesn't exist. Skipping.")
-            false
-          end
-        end
-
-        @filters = []
-
-        @config.filters.each do |filter|
-          filenames = @filter_directories.map {|dir| File.join(dir, filter.to_s + '.rb')}
-          filename = filenames.find {|filename| File.exists?(filename)}
-
-          if filename
-            @log.info("Loading the #{filter.camel_case} filter (from #{filename})")
-            require filename
-            filter = Flapjack::Filters.const_get(filter.camel_case).new(:log => @log, :persistence => @persistence)
-            @filters << filter
-          else
-            @log.warning("Flapjack::Filters::#{filter.camel_case} doesn't exist!")
-          end
-        end
-
-      end
-
-      def process_result
-        @log.debug("Waiting for new result...")
-        result = @results_queue.next # this blocks until a result is popped
-
-        @log.info("Processing result for check #{result.check_id}.")
-        event = @persistence.create_event(result)
-
-
-        block = @filters.find {|filter| filter.block?(result) }
-        unless block
-          # do munging
-          @notifier_engine.notify!(:result => result,
-                                   :event => event,
-                                   :recipients => recipients)
-        end
-
-        @log.info("Storing status of check.")
-        @persistence.save(result)
-
-        @log.info("Deleting result for check #{result.check_id}.")
-        @results_queue.delete(result)
-      end
-
-      def main
-        @log.info("Booting main loop.")
-        loop do
-          process_result
-        end
-      end
-
     end
   end
 end
