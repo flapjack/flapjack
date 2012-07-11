53c496fc3aaa9df51a774d80a5ab196ee88f7be8
Jesse Reynolds <jesse@va.com.au>
2012-07-11 17:51:02 +0930

    renamed events.feature, added events_steps

---
+++ features/steps/events_steps.rb
@@ -0,0 +1,55 @@
+#!/usr/bin/env ruby
+
+
+Given /^servce x is in an ok state$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+When /^an ok event is received for service x$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^a notification should not be generated for service x$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given /^service x is in an ok state$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+When /^a failure event is received for service x$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+When /^(\d+) seconds passes$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+When /^(\d+) minute.* passes$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^a notification should be generated for service x$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given /^service x is in scheduled maintenance$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given /^service x is in unscheduled maintenance$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+When /^an acknowledgement is received for service x$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given /^service x is in a failure state$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+When /^an acknowledgement event is received for service x$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
