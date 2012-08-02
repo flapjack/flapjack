3e83ee10d8b555bf7dd3986f64cf98d32ff75da6
Lindsay Holmwood <lindsay@bulletproof.net>
2012-08-02 15:50:22 +1000

    Use a helper method when accessing the persistence

--- lib/flapjack.rb
+++ lib/flapjack.rb
@@ -1,4 +1,27 @@
 #!/usr/bin/env ruby
 
 module Flapjack
+  def self.bootstrap(opts={})
+    if not bootstrapped?
+      defaults = {
+        :redis => {
+          :db => 0
+        }
+      }
+      @options     = defaults.merge(opts)
+
+      @@persistence = ::Redis.new(@options[:redis])
+    end
+
+    @bootstrapped = true
+  end
+
+  def self.bootstrapped?
+    @bootstrapped
+  end
+
+  def self.persistence
+    @@persistence
+  end
 end
+
