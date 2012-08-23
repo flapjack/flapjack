876d4ca4c230da421ceecc8329fc0ad93e129afb
Ali Graham <ali.graham@bulletproof.net>
2012-08-24 09:24:09 +0930

    pikelet description

--- lib/flapjack/pikelet.rb
+++ lib/flapjack/pikelet.rb
@@ -1,7 +1,12 @@
 #!/usr/bin/env ruby
 
-# Encapsulates the config loading and environment setup used
-# by the various Flapjack components
+# This class encapsulates the config data and environmental setup used
+# by the various Flapjack components.
+#
+# "In Australia and New Zealand, small pancakes (about 75 mm in diameter) known as pikelets
+# are also eaten. They are traditionally served with jam and/or whipped cream, or solely
+# with butter, at afternoon tea, but can also be served at morning tea."
+#    from http://en.wikipedia.org/wiki/Pancake
 
 require 'log4r/outputter/consoleoutputters'
 require 'log4r/outputter/syslogoutputter'
@@ -13,7 +18,7 @@ module Flapjack
     def bootstrapped?
       !!@bootstrapped
     end
-    
+
     def stop
       @should_stop = true
     end
