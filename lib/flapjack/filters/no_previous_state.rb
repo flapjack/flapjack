4cd077a64dbb022fc5c3162953a4f8de7d5828b3
Jesse Reynolds <jesse@va.com.au>
2012-08-30 09:25:27 +1000

    add no_previous_state.rb filter

---
+++ lib/flapjack/filters/no_previous_state.rb
@@ -0,0 +1,31 @@
+#!/usr/bin/env ruby
+
+require 'flapjack/filters/base'
+
+module Flapjack
+  module Filters
+
+    # * If there is no previous state for the service event, don't alert
+    class NoPreviousState
+      include Base
+
+      def block?(event)
+
+        opts = { :redis => @persistence }
+        entity_check = Flapjack::Data::EntityCheck.for_event_id(event.id, opts)
+        @log.debug("Filter NoPreviousState has created the following entity_check for #{event.id}: #{entity_check.inspect}")
+
+        if entity_check
+          @log.debug("Filter: NoPreviousState: previous state found so not blocking")
+          result = false
+        else
+          @log.debug("Filter: NoPreviousState: previous state not found so blocking")
+          result = true
+        end
+
+        @log.debug("Filter: Ok: #{result ? "block" : "pass"}")
+        result
+      end
+    end
+  end
+end
