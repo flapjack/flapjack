e85f3912875658673e6df7b8e9f90e1ec0f48423
Jesse Reynolds <jesse@bulletproof.net>
2012-08-31 15:16:31 +1000

    redis database id now configurable and working in jabber-gateway, executive, etc

--- tmp/redis_delete_all_keys.rb
+++ tmp/redis_delete_all_keys.rb
@@ -4,6 +4,6 @@ $: << File.dirname(__FILE__) + '/../lib' unless $:.include?(File.dirname(__FILE_
 
 require 'redis'
 
-@persistence = Redis.new
+@persistence = Redis.new(:db => 13)
 keys = @persistence.keys '*'
 @persistence.del(*keys)
