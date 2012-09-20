1f0b6991cf3182389f46f0cca43a6e9aa4fd7295
Jesse Reynolds <jesse@va.com.au>
2012-09-20 18:20:16 +0930

    gh-35 - oobetet implemented as a jabber bot, alerts on pagerduty and jabber if frequency of PROBLEM / RECOVERY drops below a configurable threshold

--- lib/flapjack/oobetet.rb
+++ lib/flapjack/oobetet.rb
@@ -49,6 +49,8 @@ module Flapjack
       end
 
       @check_matcher = '"' + @config['watched_check'] + '" on ' + @config['watched_entity']
+      @max_latency = @config['max_latency'] || 300
+      @flapjack_ok = true
 
       t = Time.now.to_i
       @times = { :last_problem  => t,
@@ -104,70 +106,6 @@ module Flapjack
       end
     end
 
-    def on_groupchat(stanza)
-      return if should_quit?
-      logger.debug("groupchat stanza body: " + stanza.body)
-      logger.debug("groupchat message received: #{stanza.inspect}")
-
-      if stanza.body =~ /^(\w+).*#{Regexp.escape(@check_matcher)} is (\w+)/
-        # got something interesting
-        logger.debug("groupchat found the following state for #{@check_matcher}")
-        case $1.downcase
-        when 'problem'
-          @times[:last_problem] = Time.new.to_i
-        when 'recovery'
-          @times[:last_recovery] = Time.new.to_i
-        when 'acknowledgement'
-          @times[:last_ack] = Time.new.to_i
-        end
-
-      end
-
-    end
-
-    def check_timers
-      t = Time.new.to_i
-      breach = nil
-      case
-      when @times[:last_problem]  > (t - 300)
-        breach = "haven't seen a test problem notification in the last five minutes"
-      when @times[:last_recovery] > (t - 300)
-        breach = "haven't seen a test recovery notification in the last five minutes"
-      end
-
-      return unless breach
-      @logger.error("Self monitoring has detected the following breach: #{breach}")
-      summary = "Flapjack Self Monitoring is Critical: #{breach} for #{@check_matcher}, from #{@hostname} at #{Time.now}"
-
-      if !@last_alert or @last_alert < (t - 55)
-
-        if @config['rooms'] && @config['rooms'].length > 0
-          @config['rooms'].each do |room|
-            say(room, summary, :groupchat)
-          end
-        end
-
-        if @config['jabber_contact']
-          say(@config['jabber_contact'], summary)
-          @last_alert = t
-        end
-
-        if @config['pagerduty_contact']
-          pagerduty_event = { :service_key  => @config['pagerduty_contact'],
-                              :incident_key => "Flapjack Self Monitoring from #{@hostname}",
-                              :event_type   => 'trigger',
-                              :description  => summary }
-          send_pagerduty_event(pagerduty_event)
-          @last_alert = t
-        end
-
-        if !@last_alert or @last_alert < (t - 55)
-          @logger.error("NOTICE: Self monitoring has detected a failure and is unable to tell anyone about it")
-        end
-
-      end
-    end
-
     # returning true to prevent the reactor loop from stopping
     def on_disconnect(stanza)
       return true if should_quit?
@@ -178,13 +116,96 @@ module Flapjack
       true
     end
 
+    def on_groupchat(stanza)
+      return if should_quit?
+      logger.debug("groupchat stanza body: " + stanza.body)
+      logger.debug("groupchat message received: #{stanza.inspect}")
+
+      if stanza.body =~ /^(\w+).*#{Regexp.escape(@check_matcher)}/
+        # got something interesting
+        logger.debug("groupchat found the following state for #{@check_matcher}: #{$1.downcase}")
+        case $1.downcase
+        when 'problem'
+          logger.debug("updating @times last_problem")
+          @times[:last_problem] = Time.new.to_i
+        when 'recovery'
+          logger.debug("updating @times last_recovery")
+          @times[:last_recovery] = Time.new.to_i
+        when 'acknowledgement'
+          logger.debug("updating @times last_ack")
+          @times[:last_ack] = Time.new.to_i
+        end
+
+      end
+      logger.debug("@times: #{@times.inspect}")
+
+    end
+
+    def check_timers
+      t = Time.new.to_i
+      breach = nil
+      @logger.debug("check_timers: inspecting @times #{@times.inspect}")
+      case
+      when @times[:last_problem] < (t - @max_latency)
+        breach = "haven't seen a test problem notification in the last #{@max_latency} seconds"
+      when @times[:last_recovery] < (t - @max_latency)
+        breach = "haven't seen a test recovery notification in the last #{@max_latency} seconds"
+      end
+
+      if !@flapjack_ok and !breach
+        emit_jabber("Flapjack Self Monitoring is OK")
+        emit_pagerduty("Flapjack Self Monitoring is OK", 'resolve')
+      end
+
+      @flapjack_ok = !breach
+
+      return unless breach
+      @logger.error("Self monitoring has detected the following breach: #{breach}")
+      summary  = "Flapjack Self Monitoring is Critical: #{breach} for #{@check_matcher}, "
+      summary += "from #{@hostname} at #{Time.now}"
+
+      if !@last_alert or @last_alert < (t - 55)
+
+        emit_jabber(summary)
+        emit_pagerduty(summary, 'trigger')
+
+        if !@last_alert or @last_alert < (t - 55)
+          msg  = "NOTICE: Self monitoring has detected a failure and is unable to tell "
+          msg += "anyone about it. DON'T PANIC."
+          @logger.error msg
+        end
+
+      end
+    end
+
+    def emit_jabber(summary)
+      if @config['rooms'] && @config['rooms'].length > 0
+        @config['rooms'].each do |room|
+          say(room, summary, :groupchat)
+        end
+        @last_alert = Time.now.to_i
+      end
+    end
+
+    def emit_pagerduty(summary, event_type = 'trigger')
+      if @config['pagerduty_contact']
+        pagerduty_event = { :service_key  => @config['pagerduty_contact'],
+                            :incident_key => "Flapjack Self Monitoring from #{@hostname}",
+                            :event_type   => event_type,
+                            :description  => summary }
+        status, response = send_pagerduty_event(pagerduty_event)
+        if status == 200
+          @logger.debug("successfully sent pagerduty event")
+          @last_alert = Time.now.to_i
+        else
+          @logger.error("pagerduty returned #{status} #{response.inspect}")
+        end
+      end
+    end
+
     def say(to, msg, using = :chat)
       @logger.debug("Sending a jabber message to: #{to.to_s}, using: #{using.to_s}, message: #{msg}")
       write Blather::Stanza::Message.new(to, msg, using)
-    end
-
-    def send_pagerduty_alert
-
     end
 
     def send_pagerduty_event(event)
