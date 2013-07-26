9c8f3077d4d5b14591c52460f0b49e197f32db62
Ali Graham <ali.graham@bulletproof.net>
2013-07-26 16:45:44 +0930

    more specific level control for syslog logging

--- spec/lib/flapjack/logger_spec.rb
+++ spec/lib/flapjack/logger_spec.rb
@@ -10,21 +10,21 @@ describe Flapjack::Logger do
   it "creates a logger logging to STDOUT and syslog" do
     logger.should_receive(:formatter=).with(an_instance_of(Proc))
     logger.should_receive(:level=).and_return(Logger::DEBUG)
-    logger.should_receive(:warn).with("Yowza!")
+    logger.should_receive(:add).with(2, nil, "Yowza!")
     ::Logger.should_receive(:new).with(STDOUT).and_return(logger)
 
     if Syslog.const_defined?('Logger', false)
       sys_logger.should_receive(:formatter=).with(an_instance_of(Proc))
       sys_logger.should_receive(:level=).with(Logger::DEBUG)
-      sys_logger.should_receive(:warn).with("Yowza!")
+      sys_logger.should_receive(:add).with(Logger::WARN, 'Yowza!', 'flapjack')
       Syslog.const_get('Logger', false).should_receive(:new).with('flapjack').and_return(sys_logger)
     else
-      syslog.should_receive(:log).with(Syslog::Constants::LOG_WARNING, /\[WARN\] :: spec :: %s/, "Yowza!")
-      Syslog.should_receive(:"opened?").and_return(false)
       Syslog.should_receive(:open).with('flapjack',
         (Syslog::Constants::LOG_PID | Syslog::Constants::LOG_CONS),
          Syslog::Constants::LOG_USER).and_return(syslog)
-      Syslog.should_receive(:mask=).with(Syslog::LOG_UPTO(Syslog::Constants::LOG_DEBUG))
+      Syslog.should_receive(:mask=).with(Syslog::LOG_UPTO(Syslog::Constants::LOG_WARNING))
+      Syslog.should_receive(:log).with(Syslog::Constants::LOG_WARNING, /\[WARN\] :: spec :: %s/, "Yowza!")
+      Syslog.should_receive(:close)
     end
 
     flogger = Flapjack::Logger.new('spec', 'level' => 'debug')
