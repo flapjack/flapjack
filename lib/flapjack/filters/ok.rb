a18f98a1c5d5b3493b634a67e080c431cc16ab20
Jesse Reynolds <jesse@va.com.au>
2013-08-14 13:22:03 +0930

    fixes #119 - too many recoveries during quick osciliation

--- lib/flapjack/filters/ok.rb
+++ lib/flapjack/filters/ok.rb
@@ -19,25 +19,40 @@ module Flapjack
 
         if event.ok?
           if event.previous_state == 'ok'
-            @logger.debug("Filter: Ok: existing state was ok, and the previous state was ok, so blocking")
-            result = true
+            @logger.debug("Filter: Ok: block - previous state was ok, so blocking")
+            return true
           end
 
           entity_check = Flapjack::Data::EntityCheck.for_event_id(event.id, :redis => @redis)
 
           last_notification = entity_check.last_notification
           @logger.debug("Filter: Ok: last notification: #{last_notification.inspect}")
-          if last_notification[:type] == 'recovery'
-            @logger.debug("Filter: Ok: last notification was a recovery, so blocking")
-            result = true
+
+          unless last_notification[:type]
+            @logger.debug("Filter: Ok: block - last notification type is nil (never notified)")
+            return true
           end
 
-          if event.previous_state != 'ok'
-            if event.previous_state_duration < 30
-              @logger.debug("Filter: Ok: previous non ok state was for less than 30 seconds, so blocking")
-              result = true
-            end
+          if [:recovery, :ok].include?(last_notification[:type])
+            @logger.debug("Filter: Ok: block - last notification was a recovery")
+            return true
           end
+
+          @logger.debug("Filter: Ok: previous_state: #{event.previous_state}, " +
+                        "previous_state_duration: #{event.previous_state_duration}")
+
+          # FIXME: change to if last notification was ok ?
+          #unless [:warning, :critical, :unknown, :problem, :acknowledgement].include?(last_notification[:type])
+          #  @logger.debug("Filter: Ok: block - last notification was not for a problem or acknowledgement")
+          #  return true
+          #end
+
+          #if event.previous_state != 'ok'
+          #  if event.previous_state_duration < 30
+          #    @logger.debug("Filter: Ok: previous non ok state was for less than 30 seconds, so blocking")
+          #    result = true
+          #  end
+          #end
 
           # end any unscheduled downtime
           entity_check.end_unscheduled_maintenance(Time.now.to_i)
