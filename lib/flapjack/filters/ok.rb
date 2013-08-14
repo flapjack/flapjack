dd244dcc0878334f54f7aef053b0aa9b32d7788b
Jesse Reynolds <jesse@va.com.au>
2013-08-14 13:30:54 +0930

    clean up code in ok filter

--- lib/flapjack/filters/ok.rb
+++ lib/flapjack/filters/ok.rb
@@ -5,25 +5,25 @@ require 'flapjack/filters/base'
 module Flapjack
   module Filters
 
+    # * If the service event's state is ok and there is unscheduled maintenance set, end the unscheduled
+    #   maintenance
     # * If the service event’s state is ok and the previous state was ok, don’t alert
-    # * If the service event's state is ok and the previous notification was a recovery, don't alert
-    # * If the service event's state is ok and the previous state was not ok and for less than 30
-    # seconds, don't alert
-    # * If the service event's state is ok and there is unscheduled downtime set, end the unscheduled
-    #   downtime
+    # * If the service event’s state is ok and there's never been a notification, don't alert
+    # * If the service event's state is ok and the previous notification was a recovery or ok, don't alert
     class Ok
       include Base
 
       def block?(event)
-        result = false
 
         if event.ok?
+          entity_check = Flapjack::Data::EntityCheck.for_event_id(event.id, :redis => @redis)
+
+          entity_check.end_unscheduled_maintenance(Time.now.to_i)
+
           if event.previous_state == 'ok'
             @logger.debug("Filter: Ok: block - previous state was ok, so blocking")
             return true
           end
-
-          entity_check = Flapjack::Data::EntityCheck.for_event_id(event.id, :redis => @redis)
 
           last_notification = entity_check.last_notification
           @logger.debug("Filter: Ok: last notification: #{last_notification.inspect}")
@@ -41,25 +41,10 @@ module Flapjack
           @logger.debug("Filter: Ok: previous_state: #{event.previous_state}, " +
                         "previous_state_duration: #{event.previous_state_duration}")
 
-          # FIXME: change to if last notification was ok ?
-          #unless [:warning, :critical, :unknown, :problem, :acknowledgement].include?(last_notification[:type])
-          #  @logger.debug("Filter: Ok: block - last notification was not for a problem or acknowledgement")
-          #  return true
-          #end
-
-          #if event.previous_state != 'ok'
-          #  if event.previous_state_duration < 30
-          #    @logger.debug("Filter: Ok: previous non ok state was for less than 30 seconds, so blocking")
-          #    result = true
-          #  end
-          #end
-
-          # end any unscheduled downtime
-          entity_check.end_unscheduled_maintenance(Time.now.to_i)
         end
 
-        @logger.debug("Filter: Ok: #{result ? "block" : "pass"}")
-        result
+        @logger.debug("Filter: Ok: pass")
+        return false
       end
     end
   end
