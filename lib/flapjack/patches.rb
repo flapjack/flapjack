3cb0fdefd8fedd80551c9356905b28559a151e5d
Ali Graham <ali.graham@bulletproof.net>
2013-10-17 16:20:27 +1100

    remove unneeded Redis::Future class identifier patch

--- lib/flapjack/patches.rb
+++ lib/flapjack/patches.rb
@@ -6,14 +6,6 @@ require 'redis'
 # bugfix for webrick's assuming 1.8.7 Logger syntax
 class ::Logger; alias_method :write, :<<; end
 
-# As Redis::Future objects inherit from BasicObject, it's difficult to
-# distinguish between them and other objects in collected data from
-# pipelined queries.
-#
-# (One alternative would be to put other values in Futures ourselves, and
-#  evaluate everything...)
-class Redis::Future; def class; ::Redis::Future; end; end
-
 # Pretty sure XMUC is only constructed to initialise a MUC presence,
 # so it's fine to add the extra 'no-history' stanza in any object created
 require 'xmpp4r/query'
