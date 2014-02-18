a866da78465d9cb552b0646bfb4f7c2287e2f87f
Jestin Woods <jestin.woods@jivesoftware.com>
2014-02-18 13:00:55 -0800

    Added examples of the alert erb templates to the flapjack_config.yaml example file and fixed some syntax.

--- lib/flapjack/gateways/jabber.rb
+++ lib/flapjack/gateways/jabber.rb
@@ -533,8 +533,12 @@ module Flapjack
             message_type = alert.rollup ? 'rollup' : 'alert'
 
             mydir = File.dirname(__FILE__)
-            message_template_path = @config.has_key?('templates') && @config['templates']["#{message_type}.text"] \
-              || mydir + "/jabber/#{message_type}.text.erb"
+            message_template_path = case
+            when @config.has_key?('templates') && @config['templates']["#{message_type}.text"]
+              @config['templates']["#{message_type}.text"]
+            else
+              mydir + "/jabber/#{message_type}.text.erb"
+            end
             message_template = ERB.new(File.read(message_template_path), nil, '-')
 
             @alert = alert
