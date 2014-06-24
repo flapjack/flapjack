b4dc859f8d97052e9affcfd2790d9dc562f9f6a8
Lindsay Holmwood <lindsay@holmwood.id.au>
2014-06-24 14:12:42 +1000

    Remove ability to emit the same event. Be a better cli citizen.
     - Remove looping logic (not much use submitting the same event every
       time).
     - Use flapjack.Transport for working with Redis.
     - Handle errors more consistently, exit with non-zero return code
       appropriately.

--- submit.go
+++ submit.go
@@ -3,13 +3,10 @@ package main
 import (
 	"os"
 	"fmt"
-	"log"
 	"time"
 	"flag"
-	"unicode"
 	"strings"
 	"flapjack"
-	"github.com/garyburd/redigo/redis"
 )
 
 func main() {
@@ -18,7 +15,6 @@ func main() {
 	check    := flag.String("c", "", "Check name")
 	state    := flag.String("s", "", "Current state")
 	summary  := flag.String("u", "", "Summary of event")
-	interval := flag.String("i", "0s", "How often event should be submitted e.g. 1s, 20ms")
 	debug    := flag.Bool("v", false, "Turn on debugging messages")
 	address  := flag.String("a", "localhost:6380", "Redis instance to connect to")
 	database := flag.Int("d", 0, "Redis database to connect to")
@@ -29,7 +25,6 @@ func main() {
 		fmt.Println("check:", *check)
 		fmt.Println("state:", *state)
 		fmt.Println("summary:", *summary)
-		fmt.Println("interval:", *interval)
 	}
 
 	addr := strings.Split(*address, ":")
@@ -39,14 +34,13 @@ func main() {
 		os.Exit(1)
 	}
 
-	conn, err := redis.Dial("tcp", *address) // Connect to Redis
+	transport, err := flapjack.Dial(*address, *database)
 	if err != nil {
-		fmt.Printf("Error: %s\n", err)
+		fmt.Println("Error: couldn't connect to Redis:", err)
 		os.Exit(1)
 	}
-	conn.Do("SELECT", *database) // Switch database
 
-	event := &flapjack.Event{
+	event := flapjack.Event{
 		Entity: 	*entity,
 		Check: 		*check,
 		Type:		"service",
@@ -54,25 +48,10 @@ func main() {
 		Summary:	*summary,
 		Time:		time.Now().Unix(),
 	}
-	event.Submit(conn) // Submit first event
 
-	// Determine the interval to submit events
-	i := *interval
-	if !unicode.IsLetter(rune(i[(len(i) - 1)])) {
-		i += "s"
+	_, err = transport.Send(event)
+	if err != nil {
+		fmt.Println("Error: couldn't send event:", err)
+		os.Exit(1)
 	}
-	_interval, _ := time.ParseDuration(i)
-
-	if _interval > 0 {
-		// Loop, submit, delay by interval
-		for {
-			reply := event.Submit(conn)
-			if *debug {
-				log.Println("Queue length", reply)
-				log.Println("Sleeping for", _interval)
-			}
-			time.Sleep(_interval)
-		}
-	}
-
 }
