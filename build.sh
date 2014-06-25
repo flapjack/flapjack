8b72dd87943f3382a2c49797e13eb81a7a6df911
Lindsay Holmwood <lindsay@holmwood.id.au>
2014-06-25 23:24:32 +1000

    Update build script to built tools under libexec/

--- build.sh
+++ build.sh
@@ -10,10 +10,13 @@ go get gopkg.in/alecthomas/kingpin.v1
 
 go test flapjack
 
-go build -x httpbroker.go
-go build -x submit.go
+go build -x -o libexec/httpbroker libexec/httpbroker.go
+go build -x -o libexec/oneoff libexec/oneoff.go
+
 
 if [ ! -z "$CROSSCOMPILE" ]; then
-  GOOS=linux GOARCH=amd64 CGOENABLED=0 go build -x -o submit.linux_amd64 submit.go
-  GOOS=linux GOARCH=386 CGOENABLED=0 go build -x -o submit.linux_386 submit.go
+  for command in httpbroker oneoff; do
+    GOOS=linux GOARCH=amd64 CGOENABLED=0 go build -x -o libexec/$command.linux_amd64 libexec/$command.go
+    GOOS=linux GOARCH=386 CGOENABLED=0 go build -x -o libexec/$command.linux_386 libexec/$command.go
+  done
 fi
