3dc02239e318aa0ff08576768b11be45882d06dc
Lindsay Holmwood <lindsay@holmwood.id.au>
2014-06-25 15:14:24 +1000

    Pull in packages. Run tests before build (no point building if failing)

--- build.sh
+++ build.sh
@@ -5,10 +5,13 @@ set -e
 export GOPATH=$(pwd):$GOPATH
 
 go get github.com/garyburd/redigo/redis
+go get github.com/go-martini/martini
+go get gopkg.in/alecthomas/kingpin.v1
+
+go test flapjack
 
 go build -x httpbroker.go
 go build -x submit.go
-go test flapjack
 
 if [ ! -z "$CROSSCOMPILE" ]; then
   GOOS=linux GOARCH=amd64 CGOENABLED=0 go build -x -o submit.linux_amd64 submit.go
