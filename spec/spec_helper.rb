d1850a00d4069e6792555d460e8536b09c7576d8
Ali Graham <ali.graham@bulletproof.net>
2014-07-21 13:39:09 +0930

    simplecov/oj fix

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,11 @@
 if ENV['COVERAGE']
   require 'simplecov'
-
   SimpleCov.start do
     add_filter '/spec/'
+  end
+  SimpleCov.at_exit do
+    Oj.default_options = { :mode => :compat }
+    SimpleCov.result.format!
   end
 end
 
@@ -16,16 +19,17 @@ ENV['RACK_ENV'] = ENV["FLAPJACK_ENV"]
 require 'bundler'
 Bundler.require(:default, :test)
 
+require 'oj'
+Oj.default_options = { :indent => 0, :mode => :strict }
+Oj.mimic_JSON
+require 'active_support/json'
+
 require 'webmock/rspec'
 WebMock.disable_net_connect!
 
 $:.unshift(File.dirname(__FILE__) + '/../lib')
 require 'flapjack/configuration'
 
-require 'oj'
-Oj.mimic_JSON
-Oj.default_options = { :indent => 0, :mode => :strict }
-require 'active_support/json'
 
 # Requires supporting files with custom matchers and macros, etc,
 # in ./support/ and its subdirectories.
