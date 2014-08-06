b4b165497c3fe892d1644e77eb39834f5711b105
Ali Graham <ali.graham@bulletproof.net>
2014-08-06 16:15:35 +0930

    WIP entity rename logic

--- lib/flapjack/data/entity.rb
+++ lib/flapjack/data/entity.rb
@@ -27,20 +27,130 @@ module Flapjack
         }.sort_by(&:name)
       end
 
+      def self.rename(entity_id, existing_name, entity_name, options = {})
+        raise "Redis connection not set" unless redis = options[:redis]
+
+        if redis.renamenx("entity_id:#{existing_name}", "entity_id:#{entity_name}")
+
+          # no way to lock all data operations, so hit & hope... at least the renames
+          # should be atomic
+
+          check_state_keys = redis.keys("check:#{existing_name}:*")
+
+          check_history_keys = redis.keys("#{existing_name}:*:states") +
+            redis.keys("#{existing_name}:*:state") +
+            redis.keys("#{existing_name}:*:summary") +
+            redis.keys("#{existing_name}:*:sorted_state_timestamps")
+
+          action_keys = redis.keys("#{existing_name}:*:actions")
+
+          maint_keys = redis.keys("#{existing_name}:*:*scheduled_maintenance*") +
+
+          notif_keys = redis.keys("#{existing_name}:*:last_*_notification") +
+            redis.keys("#{existing_name}:*:*_notifications")
+
+          alerting_check_keys = redis.keys("contact_alerting_checks:*")
+
+          # TODO use something that will cover disabled checks too
+          checks = redis.zrange("current_checks:#{existing_name}", 0, -1)
+
+          failed_checks    = {}
+          hashes_to_remove = []
+          hashes_to_add    = {}
+
+          alerting_to_remove = {}
+          alerting_to_add    = {}
+
+          sha1 = Digest::SHA1.new
+
+          checks.each do |ch|
+            existing_check = "#{existing_name}:#{ch}"
+            new_check      = "#{entity_name}:#{ch}"
+
+            ch_fail_score = redis.zscore("failed_checks", existing_check)
+            failed_checks[ch] = ch_fail_score unless ch_fail_score.nil?
+
+            hashes_to_remove << Digest.hexencode(sha1.digest(existing_check))[0..7].downcase
+            hashes_to_add[Digest.hexencode(sha1.digest(new_check))[0..7].downcase] = new_check
+
+            alerting_check_keys.each do |ack|
+              ack_score = redis.zscore(ack, existing_check)
+              unless ack_score.nil?
+                alerting_to_remove[ack] ||= []
+                alerting_to_remove[ack] << existing_check
+
+                alerting_to_add[ack]    ||= {}
+                alerting_to_add[ack][new_check] = ack_score
+              end
+            end
+          end
+
+          current_score = redis.zscore('current_entities', existing_name)
+
+          block_keys = redis.keys("drop_alerts_for_contact:*:*:#{existing_name}:*:*")
+
+          redis.multi
+
+          redis.hset("entity:#{entity_id}", 'name', entity_name)
+
+          check_state_keys.each do |csk|
+            redis.rename(csk, csk.sub(/^check:#{Regexp.escape(existing_name)}:/, "check:#{entity_name}:"))
+          end
+
+          (check_history_keys + action_keys + maint_keys + notif_keys).each do |chk|
+            redis.rename(chk, chk.sub(/^#{Regexp.escape(existing_name)}:/, "#{entity_name}:"))
+          end
+
+          # currently failing checks
+          failed_checks.each_pair do |ch, score|
+            redis.zrem('failed_checks', "#{existing_name}:#{ch}")
+            redis.zadd('failed_checks', score, "#{entity_name}:#{ch}")
+          end
+
+          redis.rename("current_checks:#{existing_name}", "current_checks:#{entity_name}")
+
+          unless current_score.nil?
+            redis.zrem('current_entities', existing_name)
+            redis.zadd('current_entities', current_score, entity_name)
+          end
+
+          block_keys.each do |blk|
+            redis.rename(blk, blk.sub(/^drop_alerts_for_contact:(.+):([^:]+):#{Regexp.escape(existing_name)}:(.+):([^:]+)$/,
+              "drop_alerts_for_contact:\\1:\\2:#{entity_name}:\\3:\\4"))
+          end
+
+          hashes_to_remove.each   {|hash|      redis.hdel('checks_by_hash', hash) }
+          hashes_to_add.each_pair {|hash, chk| redis.hset('checks_by_hash', hash, chk)}
+
+          alerting_to_remove.each_pair do |alerting, chks|
+            chks.each {|chk| redis.zrem(alerting, chk)}
+          end
+
+          alerting_to_add.each_pair do |alerting, chks|
+            chks.each_pair {|chk, score| redis.zadd(alerting, score, chk)}
+          end
+
+          redis.exec
+        end
+      end
+
       # NB: should probably be called in the context of a Redis multi block; not doing so
       # here as calling classes may well be adding/updating multiple records in the one
       # operation
       def self.add(entity, options = {})
         raise "Redis connection not set" unless redis = options[:redis]
-        raise "Entity name not provided" unless entity['name'] && !entity['name'].empty?
+        entity_name = entity['name']
+        raise "Entity name not provided" if entity_name.nil? || entity_name.empty?
 
-        #FIXME: should probably raise an exception if trying to create a new entity with the
-        # same name or id as an existing entity. (Go away and use update instead.)
         entity_id = entity['id'] ? entity['id'] : SecureRandom.uuid
         existing_name = redis.hget("entity:#{entity_id}", 'name')
-        redis.del("entity_id:#{existing_name}") unless existing_name == entity['name']
-        redis.set("entity_id:#{entity['name']}", entity_id)
-        redis.hset("entity:#{entity_id}", 'name', entity['name'])
+
+        if existing_name.nil?
+          redis.set("entity_id:#{entity['name']}", entity_id)
+          redis.hset("entity:#{entity_id}", 'name', entity_name)
+        elsif existing_name != entity_name
+          rename(entity_id, existing_name, entity_name, :redis => redis)
+        end
 
         redis.del("contacts_for:#{entity_id}")
         if entity['contacts'] && entity['contacts'].respond_to?(:each)
@@ -49,7 +159,7 @@ module Flapjack
             redis.sadd("contacts_for:#{entity_id}", contact_id)
           }
         end
-        self.new(:name  => entity['name'],
+        self.new(:name  => entity_name,
                  :id    => entity_id,
                  :redis => redis)
       end
--- spec/lib/flapjack/data/entity_spec.rb
+++ spec/lib/flapjack/data/entity_spec.rb
@@ -250,4 +250,322 @@ describe Flapjack::Data::Entity, :redis => true do
     expect(entities.first).to eq('def-456')
   end
 
+  context 'entity renaming' do
+
+    def do_rename
+      Flapjack::Data::Entity.add({'id'   => '5000',
+                                  'name' => 'name2',
+                                  'contacts' => ['362']},
+                                  :redis => @redis)
+    end
+
+    let(:time_i) { Time.now.to_i }
+
+    let(:sha1) { Digest::SHA1.new }
+    let(:hash_name1) { Digest.hexencode(sha1.digest('name1:PING'))[0..7].downcase }
+    let(:hash_name2) { Digest.hexencode(sha1.digest('name2:PING'))[0..7].downcase }
+
+    before(:each) do
+      Flapjack::Data::Contact.add({'id'         => '362',
+                                   'first_name' => 'John',
+                                   'last_name'  => 'Johnson',
+                                   'email'      => 'johnj@example.com' },
+                                   :redis       => @redis)
+
+      Flapjack::Data::Entity.add({'id'   => '5000',
+                                  'name' => 'name1',
+                                  'contacts' => ['362']},
+                                  :redis => @redis)
+    end
+
+    it 'renames the entity name to id lookup and the name in the entity hash by id' do
+      expect(@redis.get('entity_id:name1')).to eq('5000')
+      expect(@redis.hget('entity:5000', 'name')).to eq('name1')
+
+      do_rename
+
+      expect(@redis.get('entity_id:name1')).to be_nil
+      expect(@redis.get('entity_id:name2')).to eq('5000')
+      expect(@redis.hget('entity:5000', 'name')).to eq('name2')
+    end
+
+    it 'does not rename an entity of an entity with the new name already exists' do
+      Flapjack::Data::Entity.add({'id'   => '5001',
+                                  'name' => 'name2',
+                                  'contacts' => []},
+                                  :redis => @redis)
+
+      expect(@redis.get('entity_id:name1')).to eq('5000')
+      expect(@redis.hget('entity:5000', 'name')).to eq('name1')
+      expect(@redis.get('entity_id:name2')).to eq('5001')
+      expect(@redis.hget('entity:5001', 'name')).to eq('name2')
+
+      do_rename
+
+      # no change
+      expect(@redis.get('entity_id:name1')).to eq('5000')
+      expect(@redis.hget('entity:5000', 'name')).to eq('name1')
+      expect(@redis.get('entity_id:name2')).to eq('5001')
+      expect(@redis.hget('entity:5001', 'name')).to eq('name2')
+    end
+
+    it 'renames current check state' do
+      data = {'state' => 'critical', 'last_change' => time_i.to_s, 'last_update' => time_i.to_s}
+      @redis.mapped_hmset('check:name1:PING', data)
+
+      do_rename
+
+      expect(@redis.hgetall('check:name1:PING')).to eq({})
+      expect(@redis.hgetall('check:name2:PING')).to eq(data)
+    end
+
+    it 'renames stored check state changes' do
+      @redis.lpush('name1:PING:states', time_i)
+      @redis.set("name1:PING:#{time_i}:state", 'critical')
+      @redis.set("name1:PING:#{time_i}:summary", 'bad')
+      @redis.zadd('name1:PING:sorted_state_timestamps', time_i, time_i)
+
+      do_rename
+
+      expect(@redis.type('name1:PING:states')).to eq('none')
+      expect(@redis.type('name2:PING:states')).to eq('list')
+      expect(@redis.lindex('name2:PING:states', 0)).to eq(time_i.to_s)
+      expect(@redis.get("name1:PING:#{time_i}:state")).to be_nil
+      expect(@redis.get("name2:PING:#{time_i}:state")).to eq('critical')
+      expect(@redis.get("name1:PING:#{time_i}:summary")).to be_nil
+      expect(@redis.get("name2:PING:#{time_i}:summary")).to eq('bad')
+      expect(@redis.zrange("name1:PING:sorted_state_timestamps", 0, -1, :with_scores => true)).to eq([])
+      expect(@redis.zrange("name2:PING:sorted_state_timestamps", 0, -1, :with_scores => true)).to eq([[time_i.to_s, time_i.to_f]])
+    end
+
+    it 'renames stored action events' do
+      @redis.hset('name1:PING:actions', time_i.to_s, 'acknowledgement')
+
+      do_rename
+
+      expect(@redis.hget('name1:PING:actions', time_i.to_s)).to be_nil
+      expect(@redis.hget('name2:PING:actions', time_i.to_s)).to eq('acknowledgement')
+    end
+
+    it 'renames entries in the sorted set of failing checks' do
+      @redis.zadd('current_checks:name1', time_i, 'PING')
+      @redis.zadd('failed_checks', time_i, 'name1:PING')
+
+      do_rename
+
+      expect(@redis.zrange('failed_checks', 0, -1, :with_scores => true)).to eq([['name2:PING', time_i.to_f]])
+    end
+
+    it 'renames the list of current checks, and its entries' do
+      @redis.zadd('current_checks:name1', time_i, 'PING')
+
+      do_rename
+
+      expect(@redis.zrange('current_checks:name1', 0, -1, :with_scores => true)).to eq([])
+      expect(@redis.zrange('current_checks:name2', 0, -1, :with_scores => true)).to eq([['PING', time_i.to_f]])
+    end
+
+    it 'renames an entry in the list of current entities' do
+      @redis.zadd('current_entities', time_i, 'name1')
+
+      do_rename
+
+      expect(@redis.zrange('current_entities', 0, -1, :with_scores => true)).to eq([['name2', time_i.to_f]])
+    end
+
+    it 'renames a current unscheduled maintenance key' do
+      @redis.setex('name1:PING:unscheduled_maintenance', 30000, time_i)
+
+      do_rename
+
+      expect(@redis.get('name1:PING:unscheduled_maintenance')).to be_nil
+      expect(@redis.get('name2:PING:unscheduled_maintenance')).to eq(time_i.to_s)
+      expect(@redis.ttl('name2:PING:unscheduled_maintenance')).to be <= 30000
+    end
+
+    it 'renames stored unscheduled maintenance periods and sorted timestamps' do
+      @redis.zadd('name1:PING:unscheduled_maintenances', 30000, time_i)
+      @redis.set("name1:PING:#{time_i}:unscheduled_maintenance:summary", 'really bad')
+      @redis.zadd('name1:PING:sorted_unscheduled_maintenance_timestamps', time_i, time_i)
+
+      do_rename
+
+      expect(@redis.zrange('name1:PING:unscheduled_maintenances', 0, -1, :with_scores => true)).to eq([])
+      expect(@redis.zrange('name2:PING:unscheduled_maintenances', 0, -1, :with_scores => true)).to eq([[time_i.to_s, 30000.0]])
+      expect(@redis.get("name1:PING:#{time_i}:unscheduled_maintenance:summary")).to be_nil
+      expect(@redis.get("name2:PING:#{time_i}:unscheduled_maintenance:summary")).to eq('really bad')
+      expect(@redis.zrange('name1:PING:sorted_unscheduled_maintenance_timestamps', 0, -1, :with_scores => true)).to eq([])
+      expect(@redis.zrange('name2:PING:sorted_unscheduled_maintenance_timestamps', 0, -1, :with_scores => true)).to eq([[time_i.to_s, time_i.to_f]])
+    end
+
+    it 'renames a current scheduled maintenance key' do
+      @redis.setex('name1:PING:scheduled_maintenance', 30000, time_i)
+
+      do_rename
+
+      expect(@redis.get('name1:PING:scheduled_maintenance')).to be_nil
+      expect(@redis.get('name2:PING:scheduled_maintenance')).to eq(time_i.to_s)
+      expect(@redis.ttl('name2:PING:scheduled_maintenance')).to be <= 30000
+    end
+
+    it 'renames stored scheduled maintenance periods and sorted timestamps' do
+      @redis.zadd('name1:PING:scheduled_maintenances', 30000, time_i)
+      @redis.set("name1:PING:#{time_i}:scheduled_maintenance:summary", 'really bad')
+      @redis.zadd('name1:PING:sorted_scheduled_maintenance_timestamps', time_i, time_i)
+
+      do_rename
+
+      expect(@redis.zrange('name1:PING:scheduled_maintenances', 0, -1, :with_scores => true)).to eq([])
+      expect(@redis.zrange('name2:PING:scheduled_maintenances', 0, -1, :with_scores => true)).to eq([[time_i.to_s, 30000.0]])
+      expect(@redis.get("name1:PING:#{time_i}:scheduled_maintenance:summary")).to be_nil
+      expect(@redis.get("name2:PING:#{time_i}:scheduled_maintenance:summary")).to eq('really bad')
+      expect(@redis.zrange('name1:PING:sorted_scheduled_maintenance_timestamps', 0, -1, :with_scores => true)).to eq([])
+      expect(@redis.zrange('name2:PING:sorted_scheduled_maintenance_timestamps', 0, -1, :with_scores => true)).to eq([[time_i.to_s, time_i.to_f]])
+    end
+
+    it 'renames current notifications' do
+      @redis.set('name1:PING:last_problem_notification',         time_i)
+      @redis.set('name1:PING:last_unknown_notification',         time_i - 100)
+      @redis.set('name1:PING:last_warning_notification',         time_i - 50)
+      @redis.set('name1:PING:last_critical_notification',        time_i)
+      @redis.set('name1:PING:last_recovery_notification',        time_i - 200)
+      @redis.set('name1:PING:last_acknowledgement_notification', time_i - 250)
+
+      do_rename
+
+      expect(@redis.get('name1:PING:last_problem_notification')).to be_nil
+      expect(@redis.get('name1:PING:last_unknown_notification')).to be_nil
+      expect(@redis.get('name1:PING:last_warning_notification')).to be_nil
+      expect(@redis.get('name1:PING:last_critical_notification')).to be_nil
+      expect(@redis.get('name1:PING:last_recovery_notification')).to be_nil
+      expect(@redis.get('name1:PING:last_acknowledgement_notification')).to be_nil
+      expect(@redis.get('name2:PING:last_problem_notification')).to eq(time_i.to_s)
+      expect(@redis.get('name2:PING:last_unknown_notification')).to eq((time_i - 100).to_s)
+      expect(@redis.get('name2:PING:last_warning_notification')).to eq((time_i - 50).to_s)
+      expect(@redis.get('name2:PING:last_critical_notification')).to eq(time_i.to_s)
+      expect(@redis.get('name2:PING:last_recovery_notification')).to eq((time_i - 200).to_s)
+      expect(@redis.get('name2:PING:last_acknowledgement_notification')).to eq((time_i - 250).to_s)
+    end
+
+    it 'renames stored notifications' do
+      @redis.lpush('name1:PING:problem_notifications', time_i)
+      @redis.lpush('name1:PING:unknown_notifications', time_i - 100)
+      @redis.lpush('name1:PING:warning_notifications', time_i - 50)
+      @redis.lpush('name1:PING:critical_notifications', time_i)
+      @redis.lpush('name1:PING:recovery_notifications', time_i - 200)
+      @redis.lpush('name1:PING:acknowledgement_notifications', time_i - 250)
+
+      do_rename
+
+      expect(@redis.lindex('name1:PING:problem_notifications', 0)).to be_nil
+      expect(@redis.lindex('name1:PING:unknown_notifications', 0)).to be_nil
+      expect(@redis.lindex('name1:PING:warning_notifications', 0)).to be_nil
+      expect(@redis.lindex('name1:PING:critical_notifications', 0)).to be_nil
+      expect(@redis.lindex('name1:PING:recovery_notifications', 0)).to be_nil
+      expect(@redis.lindex('name1:PING:acknowledgement_notifications', 0)).to be_nil
+      expect(@redis.lindex('name2:PING:problem_notifications', 0)).to eq(time_i.to_s)
+      expect(@redis.lindex('name2:PING:unknown_notifications', 0)).to eq((time_i - 100).to_s)
+      expect(@redis.lindex('name2:PING:warning_notifications', 0)).to eq((time_i - 50).to_s)
+      expect(@redis.lindex('name2:PING:critical_notifications', 0)).to eq(time_i.to_s)
+      expect(@redis.lindex('name2:PING:recovery_notifications', 0)).to eq((time_i - 200).to_s)
+      expect(@redis.lindex('name2:PING:acknowledgement_notifications', 0)).to eq((time_i - 250).to_s)
+    end
+
+    it 'renames alert blocks' do
+      @redis.setex('drop_alerts_for_contact:362:email:name1:PING:critical', 30000, 30000)
+
+      do_rename
+
+      expect(@redis.get('drop_alerts_for_contact:362:email:name1:PING:critical')).to be_nil
+      expect(@redis.get('drop_alerts_for_contact:362:email:name2:PING:critical')).to eq(30000.to_s)
+      expect(@redis.ttl('drop_alerts_for_contact:362:email:name2:PING:critical')).to be <= 30000
+    end
+
+    it "updates the check hash set" do
+      @redis.zadd('current_checks:name1', time_i, 'PING')
+      @redis.hset('checks_by_hash', hash_name1, 'name1:PING')
+
+      do_rename
+
+      expect(@redis.hget('checks_by_hash', hash_name1)).to be_nil
+      expect(@redis.hget('checks_by_hash', hash_name2)).to eq('name2:PING')
+    end
+
+    # # Not a good idea, really
+
+    # it 'renames the event_id within in-flight notifications' do
+
+    #   data = {'event_id'       => 'name1:PING',
+    #           'event_hash'     => hash_name1,
+    #           'state'          => 'critical',
+    #           'summary'        => 'quite bad',
+    #           'details'        => 'not really bad',
+    #           'time'           => time_i,
+    #           'duration'       => nil,
+    #           'count'          => 50000,
+    #           'last_state'     => 'ok',
+    #           'last_summary'   => 'good',
+    #           'state_duration' => 30,
+    #           'type'           => 'problem',
+    #           'severity'       => 'critical',
+    #           'tags'           => ['name1', 'PING'] }
+
+    #   @redis.lpush('notifications', data.to_json)
+
+    #   do_rename
+
+    #   name2_data = data.merge('event_id'   => 'name2:PING',
+    #                           'event_hash' => hash_name2,
+    #                           'tags'       => ['name2', 'PING'])
+
+    #   notif = @redis.lindex('notifications', 0)
+    #   expect(notif).not_to be_nil
+    #   expect(JSON.parse(notif)).to eq(name2_data)
+    # end
+
+    # it 'renames checks within in-flight alerts' do
+    #   data = {'media'               => 'email',
+    #           'address'             => 'johnj@example.com',
+    #           'contact_id'          => '362',
+    #           'contact_first_name'  => 'John',
+    #           'contact_last_name'   => 'Johnson',
+    #           'event_id'            => 'name1:PING',
+    #           'event_hash'          => hash_name1,
+    #           'summary'             => '100% packet loss',
+    #           'duration'            => nil,
+    #           'last_state'          => 'ok',
+    #           'last_summary'        => 'ok now',
+    #           'state_duration'      => 30,
+    #           'details'             => 'hmmm',
+    #           'time'                => time_i,
+    #           'notification_type'   =>'problem',
+    #           'event_count'         => 3,
+    #           'tags'                => ['name1', 'PING']}
+
+    #   @redis.lpush('email_notifications', data.to_json)
+
+    #   do_rename
+
+    #   name2_data = data.merge('event_id'   => 'name2:PING',
+    #                           'event_hash' => hash_name2,
+    #                           'tags'       => ['name2', 'PING'])
+
+    #   notif = @redis.lindex('email_notifications', 0)
+    #   expect(notif).not_to be_nil
+    #   expect(JSON.parse(notif)).to eq(name2_data)
+    # end
+
+    it 'renames entries within alerting checks' do
+      @redis.zadd('current_checks:name1', time_i, 'PING')
+      @redis.zadd('contact_alerting_checks:362:media:email', time_i, 'name1:PING')
+
+      do_rename
+
+      expect(@redis.zrange('contact_alerting_checks:362:media:email', 0, -1, :with_scores => true)).to eq(
+        [['name2:PING', time_i.to_f]]
+      )
+    end
+
+  end
+
 end
