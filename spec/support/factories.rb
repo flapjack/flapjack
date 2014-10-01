76140a7c66cc3b1b71d39aa10abab6ea1e084cc4
Ali Graham <ali.graham@bulletproof.net>
2014-10-01 16:52:23 +0930

    fixes for sandstorm index changes

--- spec/support/factories.rb
+++ spec/support/factories.rb
@@ -19,7 +19,8 @@ module Factory
       'address' => attrs[:address], 'interval' => attrs[:interval],
       'rollup_threshold' => attrs[:rollup_threshold]}.flatten)
     redis.sadd('medium::attrs:ids', attrs[:id].to_s)
-    redis.sadd("medium::indices:by_type:#{attrs[:type]}", attrs[:type])
+    # type is known not to contain unsafe chars
+    redis.sadd("medium::indices:by_type:string:#{attrs[:type]}", attrs[:type])
 
     redis.sadd("contact:#{contact.id}:assocs:medium_ids", attrs[:id].to_s)
     redis.exec
@@ -29,7 +30,8 @@ module Factory
     redis.multi
     redis.hmset("tag:#{attrs[:id]}:attrs", {'name' => attrs[:name]}.flatten)
     redis.sadd('tag::attrs:ids', attrs[:id])
-    redis.hset("tag::indices:by_name", attrs[:name], attrs[:id])
+    name = attrs[:name].gsub(/%/, '%%').gsub(/ /, '%20').gsub(/:/, '%3A')
+    redis.hset("tag::indices:by_name", "string:#{name}", attrs[:id])
     redis.exec
   end
 
@@ -39,9 +41,11 @@ module Factory
     redis.hmset("check:#{attrs[:id]}:attrs", {'name' => attrs[:name],
       'state' => attrs[:state], 'enabled' => (!!attrs[:enabled]).to_s}.flatten)
     redis.sadd('check::attrs:ids', attrs[:id])
-    redis.hset("check::indices:by_name", attrs[:name], attrs[:id])
-    redis.sadd("check::indices:by_state:#{attrs[:state]}", attrs[:id])
-    redis.sadd("check::indices:by_enabled:#{!!attrs[:enabled]}", attrs[:id])
+    name = attrs[:name].gsub(/%/, '%%').gsub(/ /, '%20').gsub(/:/, '%3A')
+    redis.hset("check::indices:by_name", "string:#{name}", attrs[:id])
+    # state is known not to contain unsafe chars
+    redis.sadd("check::indices:by_state:string:#{attrs[:state]}", attrs[:id])
+    redis.sadd("check::indices:by_enabled:boolean:#{!!attrs[:enabled]}", attrs[:id])
 
     redis.exec
   end
