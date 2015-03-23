5318f846a782a728e46814367a09467e593abac0
Jesse Reynolds <jesse@va.com.au>
2015-03-23 15:16:39 +1030

    fixes 681 - split up metrics api endpoints

--- lib/flapjack/gateways/jsonapi/metrics_methods.rb
+++ lib/flapjack/gateways/jsonapi/metrics_methods.rb
@@ -30,10 +30,16 @@ module Flapjack
             redis.llen('events')
           end
 
+          def event_queue
+            { 'length' => redis.llen('events') }
+          end
+
           def processed_events
-            # TODO(@auxesis): potentially provide 5m,10m,30m metrics?
-            counters = Hash[redis.hgetall('event_counters').map {|k,v| [k,v.to_i] }]
-            { 'all_time' => counters }
+            Hash[redis.hgetall('event_counters').map {|k,v| [k,v.to_i] }]
+          end
+
+          def processed_events_all_time
+            { 'all_time' => processed_events }
           end
 
           def check_freshness
@@ -42,7 +48,6 @@ module Flapjack
               :redis => redis,
               :counts => true
             }
-
             Flapjack::Data::EntityCheck.find_all_split_by_freshness(freshnesses, options)
           end
 
@@ -76,11 +81,11 @@ module Flapjack
           app.helpers Flapjack::Gateways::JSONAPI::Helpers
           app.helpers Flapjack::Gateways::JSONAPI::MetricsMethods::Helpers
 
-          app.get %r{^/metrics} do
+          app.get %r{^/metrics$} do
 
             filter = params[:filter] ? filter_query(params[:filter]) : 'all'
 
-            keys = %w(fqdn pid total_keys processed_events event_queue_length check_freshness entities checks)
+            keys = %w(fqdn pid total_keys processed_events_all_time event_queue_length check_freshness entities checks)
             keys = keys.find_all {|m| filter.include?(m) } unless filter == 'all'
 
             metrics = {}
@@ -90,6 +95,27 @@ module Flapjack
 
             Flapjack.dump_json(metrics)
           end
+
+          app.get %r{^/metrics/check_freshness$} do
+            Flapjack.dump_json({"check_freshness" => check_freshness})
+          end
+
+          app.get %r{^/metrics/entities$} do
+            Flapjack.dump_json({"entities" => entities})
+          end
+
+          app.get %r{^/metrics/checks$} do
+            Flapjack.dump_json({"checks" => checks})
+          end
+
+          app.get %r{^/metrics/event_queue$} do
+            Flapjack.dump_json({"event_queue" => event_queue})
+          end
+
+          app.get %r{^/metrics/processed_events$} do
+            Flapjack.dump_json({"processed_events" => processed_events})
+          end
+
         end
 
       end
