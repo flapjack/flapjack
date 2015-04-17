ebf4ead4d2cacf15989aecbfc7ff2453c79c15aa
Ali Graham <ali.graham@bulletproof.net>
2015-04-17 13:59:02 +0930

    gofmt cleanup

--- src/flapjack/transport_test.go
+++ src/flapjack/transport_test.go
@@ -13,27 +13,27 @@ func TestDialFails(t *testing.T) {
 }
 
 func TestSendSucceeds(t *testing.T) {
-  transport, _ := Dial("localhost:6379", 9)
-  event := Event{
-    Entity:  "hello",
-    Check:   "world",
-    State:   "ok",
-    Summary: "hello world",
-  }
+	transport, _ := Dial("localhost:6379", 9)
+	event := Event{
+		Entity:  "hello",
+		Check:   "world",
+		State:   "ok",
+		Summary: "hello world",
+	}
 
-  _, err := transport.Send(event)
-  if err != nil {
-    t.Error("Error upon sending event: %v", err)
-  }
+	_, err := transport.Send(event)
+	if err != nil {
+		t.Error("Error upon sending event: %v", err)
+	}
 
 }
 
 func TestSendFails(t *testing.T) {
-  transport, _ := Dial("localhost:6379", 9)
-  event := Event{}
+	transport, _ := Dial("localhost:6379", 9)
+	event := Event{}
 
-  _, err := transport.Send(event)
-  if err == nil {
-    t.Error("Expected error upon sending event: %v", err)
-  }
+	_, err := transport.Send(event)
+	if err == nil {
+		t.Error("Expected error upon sending event: %v", err)
+	}
 }
