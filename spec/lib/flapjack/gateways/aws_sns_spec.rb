6fd4313e2201ee4ab07238f9fd955201beade5bf
Ali Graham <ali.graham@bulletproof.net>
2015-06-09 11:12:58 +0930

    further spec fix; passes locally, fails on travis

--- spec/lib/flapjack/gateways/aws_sns_spec.rb
+++ spec/lib/flapjack/gateways/aws_sns_spec.rb
@@ -9,7 +9,7 @@ describe Flapjack::Gateways::AwsSns, :logger => true do
 
   let(:time) { Time.new(2013, 10, 31, 13, 45) }
 
-  let(:time_str) { Time.at(time.to_i).utc.strftime('%Y-%m-%dT%H:%M:%SZ') }
+  let(:time_str) { '2013-10-31T03:15:00Z' }
 
   let(:config) { {'region' => 'us-east-1',
                   'access_key' => 'AKIAIOSFODNN7EXAMPLE',
