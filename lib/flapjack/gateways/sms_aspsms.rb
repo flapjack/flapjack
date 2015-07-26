28d6b6e9b037d5763f4160682a8d2d5b7f66d4a4
Moritz Kobel <moritz.kobel@itds.ch>
2015-07-26 13:34:50 +0200

    * Upgrade from Flapjack 1.6.0 to current ( used SmsMessagenet as template )

--- lib/flapjack/gateways/sms_aspsms.rb
+++ lib/flapjack/gateways/sms_aspsms.rb
@@ -1,97 +1,107 @@
 #!/usr/bin/env ruby
 
-require 'em-synchrony'
-require 'em-synchrony/em-http'
+require 'net/http'
+require 'uri'
+require 'uri/https'
+
 require 'active_support/inflector'
 
-require 'flapjack/redis_pool'
+require 'flapjack/redis_proxy'
+require 'flapjack/record_queue'
+require 'flapjack/utility'
+require 'flapjack/exceptions'
 
 require 'flapjack/data/alert'
-require 'flapjack/utility'
+require 'flapjack/data/check'
 
 module Flapjack
   module Gateways
     class SmsAspsms
 
-      ASPSMS_DEFAULT_URL = 'https://webservice.aspsms.com/aspsmsx.asmx/SimpleTextSMS'
+      ASPSMS_DEFAULT_HOST = 'webservice.aspsms.com'
+      ASPSMS_DEFAULT_PATH = '/aspsmsx.asmx/SimpleTextSMS'
       ASPSMS_DEFAULT_ORIGINATOR = 'Flapjack'
+
+      attr_accessor :sent
 
       include Flapjack::Utility
 
       def initialize(opts = {})
-        @config = opts[:config]
-        @logger = opts[:logger]
-        @redis_config = opts[:redis_config] || {}
-        @redis = Flapjack::RedisPool.new(:config => @redis_config, :size => 1, :logger => @logger)
+        @lock = opts[:lock]
 
-        @logger.info("starting")
-        @logger.debug("new sms_aspsms gateway pikelet with the following options: #{@config.inspect}")
+        @config = opts[:config]
+
+        # TODO support for config reloading
+        @queue = Flapjack::RecordQueue.new(@config['queue'] || 'sms_aspsms_notifications',
+                                           Flapjack::Data::Alert)
 
         @sent = 0
-      end
 
-      def stop
-        @logger.info("stopping")
-        @should_quit = true
-
-        redis_uri = @redis_config[:path] ||
-          "redis://#{@redis_config[:host] || '127.0.0.1'}:#{@redis_config[:port] || '6379'}/#{@redis_config[:db] || '0'}"
-        shutdown_redis = EM::Hiredis.connect(redis_uri)
-        shutdown_redis.rpush(@config['queue'], Flapjack.dump_json('notification_type' => 'shutdown'))
+        Flapjack.logger.debug("new sms aspsms gateway pikelet with the following options: #{@config.inspect}")
       end
 
       def start
-        queue = @config['queue']
+        begin
+          Zermelo.redis = Flapjack.redis
 
-        until @should_quit
-          begin
-            @logger.debug("sms_aspsms gateway is going into blpop mode on #{queue}")
-            alert = Flapjack::Data::Alert.next(queue, :redis => @redis, :logger => @logger)
-            deliver(alert) unless alert.nil?
-          rescue => e
-            @logger.error "Error generating or dispatching SMS ASPSMS message: #{e.class}: #{e.message}\n" +
-              e.backtrace.join("\n")
+          loop do
+            @lock.synchronize do
+              @queue.foreach {|alert| handle_alert(alert) }
+            end
+
+            @queue.wait
           end
+        ensure
+          Flapjack.redis.quit
         end
       end
 
-      def deliver(alert)
-        endpoint = @config["endpoint"] || ASPSMS_DEFAULT_URL
+      def stop_type
+        :exception
+      end
+
+      private
+
+      def handle_alert(alert)
+        endpoint_host = @config["endpoint_host"] || ASPSMS_DEFAULT_HOST
+        endpoint_path = @config["endpoint_path"] || ASPSMS_DEFAULT_PATH
+        originator = @config["originator"] || ASPSMS_DEFAULT_ORIGINATOR
         username = @config["username"]
         password = @config["password"]
-        originator = @config["originator"] || ASPSMS_DEFAULT_ORIGINATOR
 
-        address         = alert.address
-        notification_id = alert.notification_id
-        message_type    = alert.rollup ? 'rollup' : 'alert'
+        address = alert.medium.address
+        notification_id = alert.id
+        message_type = alert.rollup ? 'rollup' : 'alert'
+
+        sms_dir = File.join(File.dirname(__FILE__), 'sms_aspsms')
 
         sms_template_erb, sms_template =
-          load_template(@config['templates'], message_type, 'text',
-                        File.join(File.dirname(__FILE__), 'sms_aspsms'))
+            load_template(@config['templates'], message_type, 'text',
+                          File.join(File.dirname(__FILE__), 'sms_aspsms'))
 
-        @alert  = alert
-        bnd     = binding
+        @alert = alert
+        bnd = binding
 
         begin
           message = sms_template_erb.result(bnd).chomp
         rescue => e
-          @logger.error "Error while executing the ERB for an sms: " +
-            "ERB being executed: #{sms_template}"
+          Flapjack.logger.error "Error while executing the ERB for an sms: " +
+                                    "ERB being executed: #{sms_template}"
           raise
         end
 
         if @config.nil? || (@config.respond_to?(:empty?) && @config.empty?)
-          @logger.error "ASPSMS config is missing"
+          Flapjack.logger.error "Aspsms config is missing"
           return
         end
 
         errors = []
 
-        safe_message = truncate(message, 159)
+        safe_message = truncate(message, 156)
 
-        [[username, "ASPSMS username is missing"],
-         [password, "ASPSMS password is missing"],
-         [address,  "SMS address is missing"],
+        [[username, "Aspsms username is missing"],
+         [password, "Aspsms password is missing"],
+         [address, "SMS address is missing"],
          [notification_id, "Notification id is missing"]].each do |val_err|
 
           next unless val_err.first.nil? || (val_err.first.respond_to?(:empty?) && val_err.first.empty?)
@@ -99,36 +109,49 @@ module Flapjack
         end
 
         unless errors.empty?
-          errors.each {|err| @logger.error err }
+          errors.each {|err| Flapjack.logger.error err }
           return
         end
 
         query = {'UserKey'     => username,
-                 'Password'          => password,
-                 'Recipient'  => address,
+                 'Password' => password,
+                 'Recipient' => address,
                  'Originator' => originator,
                  'MessageText' => safe_message}
 
-        http = EM::HttpRequest.new(endpoint).get(:query => query)
+        # TODO ensure we're not getting a cached response from a proxy or similar,
+        # use appropriate headers etc.
+        uri = URI::HTTPS.build(:host => endpoint_host,
+                               :path => endpoint_path,
+                               :port => 443,
+                               :query => URI.encode_www_form(query))
 
-        @logger.debug "server response: #{http.response}"
+        http = Net::HTTP.new(uri.host, uri.port)
+        http.use_ssl = true
+        http.verify_mode = OpenSSL::SSL::VERIFY_PEER
+        request = Net::HTTP::Get.new(uri.request_uri)
 
-        status = (http.nil? || http.response_header.nil?) ? nil : http.response_header.status
-        if (status >= 200) && (status <= 206)
+        http_response = http.request(request)
+
+        Flapjack.logger.debug "server response: #{http_response.inspect}"
+
+        status = http_response.code
+
+        if (status.to_i >= 200) && (status.to_i <= 206)
           @sent += 1
-          alert.record_send_success!
-          @logger.debug "Sent SMS via ASPSMS, response status is #{status}, " +
-            "notification_id: #{notification_id}"
+          Flapjack.logger.info "Sent SMS via Aspsms, response status is #{status}, " +
+                                   "alert id: #{alert.id}"
         else
-          @logger.error "Failed to send SMS via ASPSMS, response status is #{status}, " +
-            "notification_id: #{notification_id}"
+          Flapjack.logger.error "Failed to send SMS via Aspsms, response status is #{status}, " +
+                                    "alert id: #{alert.id}"
         end
       rescue => e
-        @logger.error "Error generating or delivering sms to #{alert.address}: #{e.class}: #{e.message}"
-        @logger.error e.backtrace.join("\n")
+        Flapjack.logger.error "Error generating or delivering sms to #{alert.medium.address}: #{e.class}: #{e.message}"
+        Flapjack.logger.error e.backtrace.join("\n")
         raise
       end
 
     end
   end
-end                                                                                                                                    108,1         72
+end
+
