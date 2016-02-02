3d28b218d1e8422a12cf42e9b04225e7e6a996ee
Ali Graham <ali.graham@gmail.com>
2016-02-02 11:00:33 +1030

    broker push to events_actions queue for Flapjack v2

--- src/flapjack/transport.go
+++ src/flapjack/transport.go
@@ -34,15 +34,22 @@ func Dial(address string, database int) (Transport, error) {
 // Send takes an event and sends it over a transport.
 func (t Transport) Send(event Event) (interface{}, error) {
 	err := event.IsValid()
-	if err == nil {
-		data, _ := json.Marshal(event)
-		reply, err := t.Connection.Do("LPUSH", "events", data)
-		if err != nil {
-			return nil, err
-		}
-
-		return reply, nil
-	} else {
+	if err != nil {
 		return nil, err
 	}
+
+	data, _ := json.Marshal(event)
+	reply, err := t.Connection.Do("LPUSH", "events", data)
+	if err != nil {
+		return nil, err
+	}
+
+	// required for Flapjack v2 -- if the broker is split out as a separate
+	// project, this will need to be optional
+	_, err = t.Connection.Do("LPUSH", "events_actions", "+")
+	if err != nil {
+		return nil, err
+	}
+
+	return reply, nil
 }
